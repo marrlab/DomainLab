0abad3857e189623abc34e0deb0ca89a7f8fecdd
smilesun <smilesun.east@gmail.com>
2022-01-18 13:37:36 +0100

    .

--- test_out.sh
+++ test_out.sh
@@ -1,5 +1,4 @@
 python main_out.py --te_d=0 --task=mnistcolor4 --keep_model
-python main_out.py --te_d=0 --task=mnistcolor4 --aname=hduva
 python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=20
 python main_out.py --te_d=caltech --tpath=libdg/zoo/task_vlcs.py --debug --bs=20
 python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=20 --aname=deepall
