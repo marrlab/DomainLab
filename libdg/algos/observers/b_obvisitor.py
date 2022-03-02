e2aee2e28a2b2c8a847e106eb2473cafab5c1134
Xudong Sun <smilesun.east@gmail.com>
2022-03-02 20:26:19 +0100

    print2file ready

--- libdg/algos/observers/b_obvisitor.py
+++ libdg/algos/observers/b_obvisitor.py
@@ -2,21 +2,30 @@ import os
 import abc
 import torch
 
+import numpy as np
+
 from libdg.algos.observers.a_observer import AObVisitor
 from libdg.utils.utils_class import store_args
 from libdg.utils.perf import PerfClassif
 from libdg.compos.exp.exp_utils import ExpModelPersistVisitor
 
 
-def pred2file(loader_te, model, device):
+def pred2file(loader_te, model, device, fa='path_prediction.txt', flag_pred_scalar=False):
     model.eval()
     model_local = model.to(device)
     for i, (x_s, y_s, *_, path) in enumerate(loader_te):
         x_s, y_s = x_s.to(device), y_s.to(device)
         pred, *_ = model_local.infer_y_vpicn(x_s)
-        print(path)
-        breakpoint()
-
+        # print(path)
+        list_pred_list = pred.tolist()
+        list_label_list = y_s.tolist()
+        if flag_pred_scalar:
+            list_pred_list = [np.asarray(pred).argmax() for pred in list_pred_list]
+            list_label_list = [np.asarray(label).argmax() for label in list_label_list]
+        list_pair_path_pred = list(zip(path, list_label_list, list_pred_list))  # label belongs to data
+        with open(fa, 'a') as f:
+            for pair in list_pair_path_pred:
+                print(str(pair)[1:-1], file=f)  # 1:-1 removes brackets of tuple
 
 
 class ObVisitor(AObVisitor):
@@ -48,7 +57,6 @@ class ObVisitor(AObVisitor):
             acc_te = PerfClassif.cal_acc(self.host_trainer.model, self.loader_te, self.device)
             self.acc_te = acc_te
             print("out of domain test acc: ", acc_te)
-            pred2file(self.loader_te, self.host_trainer.model, self.device)
         if self.model_sel.update():
             print("model selected")
             self.exp.visitor.save(self.host_trainer.model)
@@ -73,6 +81,7 @@ class ObVisitor(AObVisitor):
         acc_te = PerfClassif.cal_acc(model_ld, self.loader_te, self.device)
         print("persisted model acc: ", acc_te)
         self.exp.visitor(acc_te)
+        pred2file(self.loader_te, self.host_trainer.model, self.device)
 
     def clean_up(self):
         """
