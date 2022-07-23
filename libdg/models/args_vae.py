4cbbd7b018fe87c398b26624d25316e288768d60
smilesun <smilesun.east@gmail.com>
2022-07-23 11:17:09 +0200

    remove default hyperpara

--- libdg/models/args_vae.py
+++ libdg/models/args_vae.py
@@ -10,13 +10,9 @@ def add_args2parser_vae(parser):
     parser.add_argument('--topic_dim', type=int, default=3,
                         help='dim latent space for topic')
 
-    # FIXME: there should not be default values of hyper-parameters,
-    # since otherwise, the user has no idea what they are doing
-
-    # FIXME: all the beta_y should be the same across the algorithms to compare the performance
-    parser.add_argument('--gamma_y', type=float, default=7e5,
+    parser.add_argument('--gamma_y', type=float, default=None,
                         help='multiplier for y classifier')
-    parser.add_argument('--gamma_d', type=float, default=1e5,
+    parser.add_argument('--gamma_d', type=float, default=None,
                         help='multiplier for d classifier from zd')
 
     # Beta VAE part
@@ -27,11 +23,12 @@ def add_args2parser_vae(parser):
     parser.add_argument('--beta_x', type=float, default=1.,
                         help='multiplier for KL x')
 
-    parser.add_argument('--beta_y', type=float, default=1000.,
+    parser.add_argument('--beta_y', type=float, default=1.,
                         help='multiplier for KL y')
+
     parser.add_argument('-w', '--warmup', type=int, default=100,
                         help='number of epochs for warm-up. \
-                        Set to 0 to turn warmup off.')
+                        Set to 0 to turn warmup off.')   # FIXME
     return parser
 
 def test_fun():
