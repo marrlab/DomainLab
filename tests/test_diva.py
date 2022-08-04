536a7995c7a841137d7459f9f862106c73f67582
smilesun <smilesun.east@gmail.com>
2022-08-04 11:19:24 +0200

    test gen diva

--- tests/test_diva.py
+++ tests/test_diva.py
@@ -4,6 +4,18 @@ from domainlab.compos.exp.exp_main import Exp
 from domainlab.arg_parser import mk_parser_main
 
 
+def test_diva():
+    parser = mk_parser_main()
+    argsstr = "--te_d=caltech --task=mini_vlcs --aname=diva --bs=2 \
+               --nname=conv_bn_pool_2 --gamma_y=7e5 --gamma_d=7e5 \
+               --nname_dom=conv_bn_pool_2 --gen"
+    margs = parser.parse_args(argsstr.split())
+    exp = Exp(margs)
+    exp.trainer.before_tr()
+    exp.trainer.tr_epoch(0)
+    exp.trainer.post_tr()
+
+
 def test_trainer_diva():
     parser = mk_parser_main()
     margs = parser.parse_args(["--te_d", "caltech",
