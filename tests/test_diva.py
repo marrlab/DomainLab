236d90b5800681368059f4de1b65a8f68bee181a
smilesun <smilesun.east@gmail.com>
2022-08-04 10:52:18 +0200

    pathlist test

--- tests/test_diva.py
+++ tests/test_diva.py
@@ -19,11 +19,29 @@ def test_trainer_diva():
     exp.trainer.tr_epoch(0)
 
 
-def test_trainer_diva_path():
+def test_trainer_diva_folder():
     testdir = os.path.dirname(os.path.realpath(__file__))
     rootdir = os.path.join(testdir, "..")
     rootdir = os.path.abspath(rootdir)
     path = os.path.join(rootdir, "examples/tasks/task_vlcs.py")
+    parser = mk_parser_main()
+    margs = parser.parse_args(["--te_d", "caltech",
+                               "--tpath", "%s" % (path),
+                               "--aname", "diva", "--bs", "2",
+                               "--nname", "conv_bn_pool_2",
+                               "--gamma_y", "7e5",
+                               "--gamma_d", "7e5",
+                               "--nname_dom", "conv_bn_pool_2"
+                               ])
+    exp = Exp(margs)
+    exp.trainer.before_tr()
+    exp.trainer.tr_epoch(0)
+
+def test_trainer_diva_pathlist():
+    testdir = os.path.dirname(os.path.realpath(__file__))
+    rootdir = os.path.join(testdir, "..")
+    rootdir = os.path.abspath(rootdir)
+    path = os.path.join(rootdir, "examples/tasks/demo_task_path_list_small.py")
     parser = mk_parser_main()
     margs = parser.parse_args(["--te_d", "caltech",
                                "--tpath", "%s" % (path),
