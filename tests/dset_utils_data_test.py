e25fb246be2ff2f724ab0d2caf8c92e9a7eb7c02
Ali <ali.shetab.boushehri@gmail.com>
2022-09-02 16:40:18 +0200

    check with codacy quality control

--- tests/dset_utils_data_test.py
+++ tests/dset_utils_data_test.py
@@ -2,8 +2,8 @@ from domainlab.dsets.dset_mnist_color_solo_default import DsetMNISTColorSoloDefa
 from domainlab.dsets.utils_data import DsetInMemDecorator
 
 
-def test_DsetInMemDecorator():
-    dset_in_memory = DsetInMemDecorator(dset=DsetMNISTColorSoloDefault( path = "../data", 
-                                                                        ind_color=1))
+def test_dset_in_mem_decorator():
+    dset = DsetMNISTColorSoloDefault(path = "../data", ind_color=1)
+    dset_in_memory = DsetInMemDecorator(dset=dset)
     dset_in_memory.__len__()
     dset_in_memory.__getitem__(0)
