3fc41fc425db89e3d0af05f38e7da44d02c06064
Ali <ali.shetab.boushehri@gmail.com>
2022-09-02 16:30:00 +0200

    create sample data

--- tests/dset_utils_data_test.py
+++ tests/dset_utils_data_test.py
@@ -1,14 +1,6 @@
-"""
-merge several solo-color mnist to form a mixed dataset
-"""
-import numpy as np
-from torch.utils.data import Dataset
 from domainlab.dsets.dset_mnist_color_solo_default import DsetMNISTColorSoloDefault
-from domainlab.dsets.utils_data import mk_fun_label2onehot
-from domainlab.dsets.utils_data import plot_ds, plot_ds_list
-from domainlab.dsets.dset_poly_domains_mnist_color_default import DsetMNISTColorMix
 from domainlab.dsets.utils_data import DsetInMemDecorator
 
 
 def test_DsetInMemDecorator():
-    dset_in_memory = DsetInMemDecorator(None)
+    dset_in_memory = DsetInMemDecorator(dset=DsetMNISTColorSoloDefault(path = "../data", ind_color=1))
