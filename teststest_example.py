f5fac5da43e10ade6cc5af3e9561029780e75836
doparmin <71218473+doparmin@users.noreply.github.com>
2022-09-09 17:52:51 +0200

    Create teststest_example.py

---
+++ teststest_example.py
@@ -0,0 +1,4 @@
+from tests.utils_test import utils_test_algo
+
+def test_example():
+    utils_test_algo("--te_d=0 --task=mnistcolor10 --keep_model --aname=diva --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 --gamma_y=10e5 --gamma_d=1e5 --gen")
