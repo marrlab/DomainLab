ebd55003d27b84d6073a4281070e81053088c445
Rao Muhammad Umer <engr.raoumer943@gmail.com>
2022-09-12 11:07:49 +0200

    Update test_densenet.py

--- tests/test_densenet.py
+++ tests/test_densenet.py
@@ -11,13 +11,22 @@ from domainlab.compos.nn_zoo.net_conv_conv_bn_pool_2 import NetConvDense
 
 
 def test_netconvdense1():
+    """
+    test convdensenet
+    """
     inpu = torch.randn(1, 3, 28, 28)
-    model = NetConvDense(i_c=3, i_h=28, i_w=28, conv_stride=1, dim_out_h=32, args=None, dense_layer=None)
+    model = NetConvDense(i_c=3, i_h=28, i_w=28,\
+                         conv_stride=1, dim_out_h=32,\
+                         args=None, dense_layer=None)
     model(inpu)
-    
 
 def test_netconvdense2():
-    inpu = torch.randn(1,3,28,28)
+    """
+    test convdensenet
+    """
+    inpu = torch.randn(1, 3, 28, 28)
     dense_layers = DenseNet(1024, out_hidden_size=32)
-    model = NetConvDense(i_c=3, i_h=28, i_w=28, conv_stride=1, dim_out_h=32, args=None, dense_layer=dense_layers)
+    model = NetConvDense(i_c=3, i_h=28, i_w=28,\
+                         conv_stride=1, dim_out_h=32,\
+                         args=None, dense_layer=dense_layers)
     model(inpu)
