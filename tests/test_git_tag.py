8b1bee0c5850254f611b689a21c6c84d337d7bf3
Rao Muhammad Umer <engr.raoumer943@gmail.com>
2022-09-12 11:09:39 +0200

    Update test_git_tag.py

--- tests/test_git_tag.py
+++ tests/test_git_tag.py
@@ -4,10 +4,10 @@ Code coverage issues:
     - lines 10-20
     - lines 28, 30-32
 '''
-
 from domainlab.utils.get_git_tag import get_git_tag
 
-
 def test_git_tag():
-    git_tag = get_git_tag(print_diff=True)
-    print(git_tag)
+    """
+    test git_tag
+    """
+    get_git_tag(print_diff=True)
