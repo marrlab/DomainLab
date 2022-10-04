ad4ae8cca784d7718a897280fbfd717ff31772ac
smilesun <smilesun.east@gmail.com>
2022-10-04 17:25:22 +0200

    add slurm script

---
+++ slurm_script/pacs/readme.txt
@@ -0,0 +1,1 @@
+sh submit_jobs_algos_cluster.sh 0 9   # random seed start with 0 end with 8
---
+++ slurm_script/pacs/submit_jobs_algos_cluster.sh
@@ -0,0 +1,26 @@
+repodir="../.."
+echo "source code directory is $repodir"
+echo "seed range: $1:$2"
+startseed=$1
+endseed=$2
+mkdir -p logs
+currenttime="`date`"
+
+scriptname='sbatch_pacs_dir_algo_start_end_seed_epos.sh'
+epos=300  # Let hduva and diva run enough epochs
+
+batchsize="20"  # FIXME
+paths="--tpath=examples/tasks/not_a_demo_task_path_list.py --npath=examples/nets/resnet.py"   # FIXME
+algoconf="sh_algo_conf_cluster.sh"   # FIXME
+
+#
+
+echo "current time: $currenttime"
+
+# each job correspond to one algorithm
+for algo in "hduva" "diva" "matchdg" "deepall" "dann"
+do
+    echo "algo: $algo"
+    JOBNAME="${algo}${currenttime}"
+    sbatch --error="logs/${JOBNAME}_error_${currenttime}" --output="logs/${JOBNAME}_output_${currenttime}" --job-name="${JOBNAME}" $scriptname $repodir $algo $startseed $endseed $epos $algoconf "$paths" "$batchsize"
+done
