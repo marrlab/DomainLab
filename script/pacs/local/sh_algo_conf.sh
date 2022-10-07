c22e470bcfd6a9f0051171c12192d03636667dbf
smilesun <smilesun.east@gmail.com>
2022-10-07 08:08:08 +0200

    remote script

--- script/pacs/local/sh_algo_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -A dict_algo_args
-dict_algo_args['diva']='--npath_dom=examples/nets/resnet.py --gamma_y=1e5 --gamma_d=1e5'
-dict_algo_args['matchdg']='--epochs_ctr=1 --epochs_erm=1'
-dict_algo_args['hduva']='--gamma_y=1e5 --nname_topic_distrib_img2topic=conv_bn_pool_2 --nname_encoder_sandwich_layer_img2h4zd=conv_bn_pool_2'
-dict_algo_args['deepall']=''
-# echo "${dict_algo_args['diva']}"
-
-
