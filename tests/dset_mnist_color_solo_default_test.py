69825d7797a340ef80e090396a2efbdcfb4e5a1f
smilesun <smilesun.east@gmail.com>
2022-12-06 17:29:17 +0100

    add test

--- tests/dset_mnist_color_solo_default_test.py
+++ tests/dset_mnist_color_solo_default_test.py
@@ -13,3 +13,15 @@ def test_color_mnist():
     for i in range(10):
         ds_list.append(DsetMNISTColorSoloDefault(i, "zout"))
     plot_ds_list(ds_list, "zout/color_0_9.png")
+
+
+def test_color_mnist2():
+    """
+    test_color_mnist
+    """
+    dset = DsetMNISTColorSoloDefault(0, "zout", flag_rand_color=True)
+    plot_ds(dset, "zout/color_solo.png")
+    ds_list = []
+    for i in range(10):
+        ds_list.append(DsetMNISTColorSoloDefault(i, "zout"))
+    plot_ds_list(ds_list, "zout/color_0_9.png")
