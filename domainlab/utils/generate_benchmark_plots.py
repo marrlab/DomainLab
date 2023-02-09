9331212835092cadbb35cf0126521e13bf440a06
carla <carla.feistner@tum.de>
2023-02-09 13:08:31 +0100

    wrote gen_benchmark_plots for plots 3 and 4

--- domainlab/utils/generate_benchmark_plots.py
+++ domainlab/utils/generate_benchmark_plots.py
@@ -1,8 +1,112 @@
+import os
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import Union, List, Optional, Tuple
+from ast import literal_eval
+
+from numpy.linalg import LinAlgError
+
+
+def gen_benchmark_plots(agg_results: str, output_dir: str):
+    '''
+    generate the benchmark plots from a csv file containing the aggregated restults.
+    The csv file must have the columns:
+    [param_index, task, algo, epos, te_d, seed, params, ...]
+    all columns after seed are intrepreted as objectives of the results, they can e.g.
+    be acc, precision, recall, specificity, f1, auroc.
+
+    agg_results: path to the csv file
+    output_dir: path to a folder which shall contain the results
+    '''
+    df = pd.read_csv(agg_results, index_col=False, converters={'params': literal_eval}, skipinitialspace=True)
+    # crop param_index and task from the dataframe
+    dataframe = df.iloc[:, 2:]
+    gen_plots(dataframe, output_dir)
+
+
+
+def gen_plots(dataframe: pd.DataFrame, output_dir: str):
+    '''
+    dataframe: dataframe with columns [' algo', ' epos', ' te_d', ' seed', ' params', ' acc', ' precision', ... ]
+    '''
+    os.makedirs(output_dir, exist_ok=True)
+
+    #scatterplot matrices
+    scatterplot_matrix(dataframe, file=output_dir + '/sp_matrix_reg.png', reg=True, distinguish_param_setups=False)
+    scatterplot_matrix(dataframe, file=output_dir + '/sp_matrix.png', reg=False, distinguish_param_setups=False)
+    scatterplot_matrix(dataframe, file=output_dir + '/sp_matrix_dist_reg.png', reg=True, distinguish_param_setups=True)
+    scatterplot_matrix(dataframe, file=output_dir + '/sp_matrix_dist.png', reg=False, distinguish_param_setups=True)
+
+    # radar plots
+    radar_plot(dataframe, file=output_dir + '/radar_dist.png', distinguish_hyperparam=True)
+    radar_plot(dataframe, file=output_dir + '/radar.png', distinguish_hyperparam=False)
+
+    # scatter plots for parirs of objectives
+    os.makedirs(output_dir + '/scatterpl', exist_ok=True)
+    obj = dataframe.columns[5:]
+    for i in range(len(obj)):
+       for j in range(i+1, len(obj)):
+           try:
+               scatterplot(dataframe, obj[i], obj[j],
+                           file=output_dir + '/scatterpl/' + obj[i] + '_' + obj[j] + '.png')
+           except LinAlgError:
+               print(f'WARNING: disabling kde because cov matrix is singular for objectives '
+                     f'{obj[i]} & {obj[j]}')
+               scatterplot(dataframe, obj[i], obj[j],
+                           file=output_dir + '/scatterpl/' + obj[i] + '_' + obj[j] + '.png',
+                           kde=False)
+
+    # create plots for the different algortihms
+    for algorithm in dataframe['algo'].unique():
+        os.makedirs(output_dir + '/' + str(algorithm), exist_ok=True)
+        dataframe_algo = dataframe[dataframe['algo'] == algorithm]
+        # scatterplot matrices
+        scatterplot_matrix(dataframe_algo,
+                           file=output_dir + '/' + str(algorithm) + '/sp_matrix_reg.png',
+                           reg=True, distinguish_param_setups=False)
+        scatterplot_matrix(dataframe_algo,
+                           file=output_dir + '/' + str(algorithm) + '/sp_matrix.png',
+                           reg=False, distinguish_param_setups=False)
+        scatterplot_matrix(dataframe_algo,
+                           file=output_dir + '/' + str(algorithm) + '/sp_matrix_dist_reg.png',
+                           reg=True, distinguish_param_setups=True)
+        scatterplot_matrix(dataframe_algo,
+                           file=output_dir + '/' + str(algorithm) + '/sp_matrix_dist.png',
+                           reg=False, distinguish_param_setups=True)
+
+        # radar plots
+        radar_plot(dataframe_algo,
+                   file=output_dir + '/' + str(algorithm) + '/radar_dist.png',
+                   distinguish_hyperparam=True)
+        radar_plot(dataframe_algo,
+                   file=output_dir + '/' + str(algorithm) + '/radar.png',
+                   distinguish_hyperparam=False)
+
+        # scatter plots for parirs of objectives
+        os.makedirs(output_dir + '/' + str(algorithm) + '/scatterpl', exist_ok=True)
+        obj = dataframe_algo.columns[5:]
+        for i in range(len(obj)):
+            for j in range(i + 1, len(obj)):
+                try:
+                    scatterplot(dataframe_algo, obj[i], obj[j],
+                                file=output_dir + '/' + str(algorithm) +
+                                     '/scatterpl/' + obj[i] + '_' + obj[j] + '.png',
+                                distinguish_hyperparam=True)
+                except LinAlgError:
+                    print(f'WARNING: disabling kde because cov matrix is singular for objectives '
+                          f'{obj[i]} & {obj[j]}')
+                    scatterplot(dataframe_algo, obj[i], obj[j],
+                                file=output_dir + '/' + str(algorithm) +
+                                     '/scatterpl/' + obj[i] + '_' + obj[j] + '.png',
+                                kde=False,
+                                distinguish_hyperparam=True)
+
+
+
+
 
 def scatterplot_matrix(dataframe_in, file=None, reg=True, distinguish_param_setups=True):
     '''
@@ -19,7 +123,7 @@ def scatterplot_matrix(dataframe_in, file=None, reg=True, distinguish_param_setu
         dataframe_ = dataframe.iloc[:, index]
         dataframe_.insert(0, 'label',
                           dataframe['algo'].astype(str) + ', ' +
-                          dataframe['hyperparameter'].astype(str))
+                          dataframe['params'].astype(str))
     else:
         index_ = list(range(5, dataframe.shape[1]))
         index_.insert(0, 0)
@@ -39,83 +143,49 @@ def scatterplot_matrix(dataframe_in, file=None, reg=True, distinguish_param_setu
     for i in range(len(index)):
         for j in range(len(index)):
             if i >= j:
-                label = dataframe_.columns[j + 1]
-                dist = 0.1 * (max(dataframe_[label]) - min(dataframe_[label]))
-                #g.axes[i, j].set_xlim((min(dataframe_[label]) - dist,
-                #                       max(dataframe_[label]) + dist))
                 g.axes[i, j].set_xlim((-0.1, 1.1))
                 for k in range(j):
-                    #g.axes[j, k].set_ylim((min(dataframe_[label]) - dist,
-                    #                       max(dataframe_[label]) + dist))
                     g.axes[j, k].set_ylim((-0.1, 1.1))
+
+    g.fig.set_size_inches(12.5, 12)
+    sns.move_legend(g, loc='upper right', bbox_to_anchor=(1., 1.), ncol=1)
     plt.tight_layout()
 
     if file is not None:
         plt.savefig(file, dpi=300)
 
 
-def scatterplot_errorous(dataframe_in, obj1, obj2, file=None,distinguish_param_setups=True):
-    dataframe = dataframe_in.copy()
-    dataframe['hyperparameter'] = dataframe['hyperparameter'].astype(str)
 
-    if distinguish_param_setups:
-        dataframe.insert(0, 'label',
-                         dataframe['algo'].astype(str) + ', ' +
-                         dataframe['hyperparameter'].astype(str))
+def scatterplot(dataframe_in, obj1, obj2, file=None, kde=True, distinguish_hyperparam=False):
+    dataframe = dataframe_in.copy()
+    dataframe['params'] = dataframe['params'].astype(str)
+
+    if distinguish_hyperparam:
+        if kde:
+            g = sns.jointplot(data=dataframe, x=obj1, y=obj2, hue='params',
+                              xlim=(-0.1, 1.1), ylim=(-0.1, 1.1), kind='kde',
+                              zorder=0, levels=8, alpha=0.35)
+            gg = sns.scatterplot(data=dataframe, x=obj1, y=obj2, hue='params',
+                                 ax=g.ax_joint)
+        else:
+            g = sns.jointplot(data=dataframe, x=obj1, y=obj2, hue='params',
+                              xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
+            gg = g.ax_joint
     else:
-        dataframe.insert(0, 'label', dataframe['algo'])
+        if kde:
+            g = sns.jointplot(data=dataframe, x=obj1, y=obj2, hue='algo',
+                              xlim=(-0.1, 1.1), ylim=(-0.1, 1.1), kind='kde',
+                              zorder=0, levels=8, alpha=0.35)
+            gg = sns.scatterplot(data=dataframe, x=obj1, y=obj2, hue='algo', style='params',
+                                 ax=g.ax_joint)
+        else:
+            g = sns.jointplot(data=dataframe, x=obj1, y=obj2, hue='algo',
+                              xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
+            gg = sns.scatterplot(data=dataframe, x=obj1, y=obj2, style='params',
+                                 ax=g.ax_joint)
 
-    mean_df = dataframe.groupby('label').mean()
-    std_df = dataframe.groupby('label').std()
-
-
-
-    plt.figure(figsize=(9, 6))
-    plt.axes().set_aspect('equal')
-
-    g = sns.scatterplot(data=mean_df, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    mean_df[obj1] = mean_df[obj1] + std_df[obj1]
-    g.scatterplot(data=mean_df, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    mean_df[obj1] = mean_df[obj1] - 2 * std_df[obj1]
-    g.scatterplot(data=mean_df, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    mean_df[obj1] = mean_df[obj1] + std_df[obj1]
-    mean_df[obj2] = mean_df[obj2] + std_df[obj2]
-    g.scatterplot(data=mean_df, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    mean_df[obj2] = mean_df[obj2] - 2 * std_df[obj2]
-    g.scatterplot(data=mean_df, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    g.legend(loc='center left', bbox_to_anchor=(1.1, 0.5), ncol=1)
-
-
-    g.axes.set_xlim((-0.1, 1.1))
-    g.axes.set_ylim((-0.1, 1.1))
-
-    plt.tight_layout()
-
-    if file is not None:
-        plt.savefig(file, dpi=300)
-
-def scatterplot(dataframe_in, obj1, obj2, file=None):
-    dataframe = dataframe_in.copy()
-    dataframe['hyperparameter'] = dataframe['hyperparameter'].astype(str)
-
-    plt.figure(figsize=(7.5, 5))
-    plt.axes().set_aspect('equal')
-
-    sns.kdeplot(data=dataframe, x=obj1, y=obj2, hue='algo',
-                alpha=0.35, levels=8)
-    g = sns.scatterplot(data=dataframe, x=obj1, y=obj2,
-                        hue='algo', style='hyperparameter')
-    g.legend(loc='upper left', bbox_to_anchor=(1.01, 1.), ncol=1)
-
-    g.axes.set_xlim((-0.1, 1.1))
-    g.axes.set_ylim((-0.1, 1.1))
-
-    plt.tight_layout()
+    gg.set_aspect('equal')
+    g.ax_joint.legend(fontsize=6)
 
     if file is not None:
         plt.savefig(file, dpi=300)
@@ -129,7 +199,7 @@ def radar_plot(dataframe_, file=None, distinguish_hyperparam=True):
     if distinguish_hyperparam:
         dataframe.insert(0, 'label',
                          dataframe['algo'].astype(str) + ', ' +
-                         dataframe['hyperparameter'].astype(str))
+                         dataframe['params'].astype(str))
     else:
         dataframe.insert(0, 'label', dataframe['algo'])
     index = list(range(6, dataframe.shape[1]))
@@ -154,12 +224,6 @@ def radar_plot(dataframe_, file=None, distinguish_hyperparam=True):
         ax.plot(angles_, mean,
                 color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
                 linewidth=2, label=algo_name)
-        #ax.plot(angles_, np.array(list(map(max_0_x, mean - std))),
-        #        color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-        #        linewidth=0.7)
-        #ax.plot(angles_, np.array(list(map(max_0_x, mean + std))),
-        #        color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-        #        linewidth=0.7)
 
         # Fill it in.
         ax.fill_between(angles_, list(map(max_0_x, mean - std)), y2 = mean + std,
@@ -177,68 +241,7 @@ def radar_plot(dataframe_, file=None, distinguish_hyperparam=True):
 
     ax.set_ylim((0, 1))
 
-    plt.legend()
-    plt.tight_layout()
-
-    if file is not None:
-        plt.savefig(file, dpi=300)
-
-
-def radar_plot_per_algo(dataframe_, file=None):
-    dataframe = dataframe_.copy()
-    dataframe.insert(0, 'label',
-                     dataframe['algo'].astype(str) + ', ' +
-                     dataframe['hyperparameter'].astype(str))
-    index = list(range(6, dataframe.shape[1]))
-    _, ax = plt.subplots(1, len(list(dataframe['algo'].unique())),
-                         figsize=(6 * len(list(dataframe['algo'].unique())), 6),
-                         subplot_kw=dict(polar=True))
-
-    # Split the circle into even parts and save the angles
-    # so we know where to put each axis.
-    angles = list(np.linspace(0, 2 * np.pi, len(dataframe.columns[index]), endpoint=False))
-    for idx, algo_name in enumerate(list(dataframe['algo'].unique())):
-        algo_dataframe = dataframe.loc[dataframe['algo'] == algo_name]
-        num = 0
-
-        for label in algo_dataframe['label'].unique():
-            mean = dataframe.loc[dataframe['label'] == label].iloc[:, index].mean().to_list()
-            std = dataframe.loc[dataframe['label'] == label].iloc[:, index].std().to_list()
-
-            angles_ = angles
-            # The plot is a circle, so we need to "complete the loop"
-            # and append the start value to the end.
-            mean = np.array(mean + mean[:1])
-            std = np.array(std + std[:1])
-            angles_ = np.array(angles_ + angles_[:1])
-
-            # Draw the outline of the data.
-            ax[idx].plot(angles_, mean,
-                    color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-                    linewidth=2, label=label)
-            #ax[idx].plot(angles_, np.array(list(map(max_0_x, mean - std))),
-            #        color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-            #        linewidth=0.7)
-            #ax[idx].plot(angles_, np.array(list(map(max_0_x, mean + std))),
-            #        color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-            #        linewidth=0.7)
-
-            # Fill it in.
-            ax[idx].fill_between(angles_, list(map(max_0_x, mean - std)), y2=mean + std,
-                            color=list(plt.rcParams["axes.prop_cycle"])[num]['color'],
-                            alpha=0.1)
-            num += 1
-            num = num % len(list(plt.rcParams["axes.prop_cycle"]))
-
-            # Fix axis to go in the right order and start at 12 o'clock.
-            ax[idx].set_theta_offset(np.pi / 2)
-            ax[idx].set_theta_direction(-1)
-
-            # Draw axis lines for each angle and label.
-            ax[idx].set_thetagrids(np.degrees(angles), dataframe.columns[index])
-
-            ax[idx].set_ylim((0, 1))
-            ax[idx].legend()
+    plt.legend(loc='upper left', bbox_to_anchor=(1., 1.), ncol=1)
     plt.tight_layout()
 
     if file is not None:
@@ -255,12 +258,12 @@ def box_plot(
 ):
     if hyperparamter_filter:
         pass
-    data['hyperparameters'] = data['hyperparameters'].astype(str)
+    data['params'] = data['params'].astype(str)
     if isinstance(metrics, str):
         metrics = [metrics]
     num_plots = len(metrics) * (int(plot_pooled == True) + int(groupby_hyperparamter == True))
     print(num_plots)
-    data = data[['algo', 'epos', 'seed', 'hyperparameters'] + metrics]
+    data = data[['algo', 'epos', 'seed', 'params'] + metrics]
     if figsize is None:
         #figsize =
         pass
@@ -271,7 +274,7 @@ def box_plot(
             sns.boxplot(data=data, x="algo", y=item)
         if groupby_hyperparamter:
             plt.subplot(num_plots, 1, 2*i)
-            sns.boxplot(data=data, x="algo", y=item, hue='hyperparameters')
+            sns.boxplot(data=data, x="algo", y=item, hue='params')
         fig.tight_layout()
         plt.show()
 
@@ -304,7 +307,7 @@ if __name__ == '__main__':
               {'c': -1, 'b':0}, 0.72422227, 0.20658247, 0.2084048, 0.7009563, 0.18635866, 0.61927835]
 
 
-    # dataset containing results from 2 algorithms, both were run with two different initialisations of the hyperparameters
+    # dataset containing results from 2 algorithms, both were run with two different initialisations of the params
     # for each hyperparameter initialisation three runs with different random seeds are included
     dummy_dataframe = pd.DataFrame([diva01, diva02, diva03, diva11, diva12, diva13,
                                     hduva01, hduva02, hduva03, hduva11, hduva12, hduva13],
@@ -312,25 +315,5 @@ if __name__ == '__main__':
                  'acc', 'precision', 'recall', 'specificity', 'f1', 'auroc'])
 
 
-    #print(dummy_dataframe)
 
-    #scatterplot_matrix(dummy_dataframe, file='0smatrix_reg.png', reg=True, distinguish_param_setups=False)
-    #scatterplot_matrix(dummy_dataframe, file='0smatrix.png', reg=False, distinguish_param_setups=False)
-    #scatterplot_matrix(dummy_dataframe, file='1smatrix_reg_dist.png', reg=True, distinguish_param_setups=True)
-    #scatterplot_matrix(dummy_dataframe, file='1smatrix_dist.png', reg=False, distinguish_param_setups=True)
-
-    radar_plot(dummy_dataframe, file='3radar.png', distinguish_hyperparam=True)
-    radar_plot(dummy_dataframe, file='2radar_dist.png', distinguish_hyperparam=False)
-    radar_plot_per_algo(dummy_dataframe, file='4radar_per_algo.png')
-
-    obj = ['acc', 'precision', 'recall', 'specificity', 'f1', 'auroc']
-    for i in range(len(obj)):
-        for j in range(i+1, len(obj)):
-            obj1 = obj[i]
-            obj2 = obj[j]
-            scatterplot(dummy_dataframe, obj1, obj2,
-                        file='scatterpl/' + obj1 + '_' + obj2 + '.png')
-
-    plt.show()
-
-
+    gen_benchmark_plots('aggret_res_test2', 'outp2')
