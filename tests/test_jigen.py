6529c6ae71f699dfa3942afdbd44b51c2d5bd462
smilesun <smilesun.east@gmail.com>
2023-02-12 17:37:55 +0100

    forgot end2end test upload

---
+++ tests/test_jigen.py
@@ -0,0 +1,17 @@
+from domainlab.compos.exp.exp_main import Exp
+from domainlab.arg_parser import mk_parser_main
+
+
+def test_trainer_diva():
+    parser = mk_parser_main()
+    margs = parser.parse_args(["--te_d", "caltech",
+                               "--task", "mini_vlcs",
+                               "--aname", "hduva", "--bs", "2",
+                               "--nname", "conv_bn_pool_2",
+                               "--gamma_y", "7e5",
+                               "--nname_topic_distrib_img2topic", "conv_bn_pool_2",
+                               "--nname_encoder_sandwich_layer_img2h4zd", "alexnet"
+                               ])
+    exp = Exp(margs)
+    exp.trainer.before_tr()
+    exp.trainer.tr_epoch(0)
