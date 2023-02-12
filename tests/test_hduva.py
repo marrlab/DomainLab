c2f4a5a10f66b5288b0d428481467417ecfde47d
smilesun <smilesun.east@gmail.com>
2023-02-12 18:02:30 +0100

    switch wrong filename

--- tests/test_hduva.py
+++ tests/test_hduva.py
@@ -5,15 +5,18 @@ from domainlab.compos.exp.exp_main import Exp
 from domainlab.arg_parser import mk_parser_main
 
 
-def test_trainer_jigen():
+def test_trainer_hduva():
     """
     end to end test
     """
     parser = mk_parser_main()
     margs = parser.parse_args(["--te_d", "caltech",
                                "--task", "mini_vlcs",
-                               "--aname", "jigen", "--bs", "2",
-                               "--nname", "alexnet"
+                               "--aname", "hduva", "--bs", "2",
+                               "--nname", "conv_bn_pool_2",
+                               "--gamma_y", "7e5",
+                               "--nname_topic_distrib_img2topic", "conv_bn_pool_2",
+                               "--nname_encoder_sandwich_layer_img2h4zd", "alexnet"
                                ])
     exp = Exp(margs)
     exp.trainer.before_tr()
--- tests/test_jigen.py
+++ tests/test_jigen.py
@@ -1,16 +1,20 @@
+"""
+end to end test, each file only test only 1 algorithm
+so it is easier to identify which algorithm has a problem
+"""
 from domainlab.compos.exp.exp_main import Exp
 from domainlab.arg_parser import mk_parser_main
 
 
-def test_trainer_diva():
+def test_trainer_jigen():
+    """
+    end to end test
+    """
     parser = mk_parser_main()
     margs = parser.parse_args(["--te_d", "caltech",
                                "--task", "mini_vlcs",
-                               "--aname", "hduva", "--bs", "2",
-                               "--nname", "conv_bn_pool_2",
-                               "--gamma_y", "7e5",
-                               "--nname_topic_distrib_img2topic", "conv_bn_pool_2",
-                               "--nname_encoder_sandwich_layer_img2h4zd", "alexnet"
+                               "--aname", "jigen", "--bs", "2",
+                               "--nname", "alexnet"
                                ])
     exp = Exp(margs)
     exp.trainer.before_tr()
