83eb17c9203ca2f455b78e6b45d4e2af09a9dad8
Xudong Sun <smilesun@users.noreply.github.com>
2023-02-13 11:38:53 +0100

    let jigen run 20 epochs t
    to check if the permutation classifier works correctely

--- tests/test_jigen.py
+++ tests/test_jigen.py
@@ -14,6 +14,7 @@ def test_trainer_jigen():
     margs = parser.parse_args(["--te_d", "caltech",
                                "--task", "mini_vlcs",
                                "--aname", "jigen", "--bs", "2",
+                               "--epos", "20",
                                "--nname", "alexnet"])
     exp = Exp(margs)
     exp.trainer.before_tr()
