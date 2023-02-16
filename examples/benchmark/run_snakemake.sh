fc8c600e10168e2e9a06f6956660919ef0f1bac6
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-16 14:43:03 +0100

    add --debug option for benchmark.smk

--- examples/benchmark/run_snakemake.sh
+++ examples/benchmark/run_snakemake.sh
@@ -6,7 +6,8 @@
 # -- configfile: configuration yaml file of the benchmark
 
 #snakemake -np -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
-snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
+#snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
+snakemake --debug --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 #snakemake --rerun-incomplete --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 
 
