042a1fe80d36c2c8f0c3fb585963646c626c1b62
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-17 12:36:42 +0100

    denbi config

--- examples/benchmark/run_snakemake.sh
+++ examples/benchmark/run_snakemake.sh
@@ -6,10 +6,14 @@
 # -- configfile: configuration yaml file of the benchmark
 
 #snakemake -np -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
-snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
+#snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 #snakemake --rerun-incomplete --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 
 
 # print execution graph to pdf
 #snakemake --dag --forceall -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" | dot -Tpdf > dag.pdf
 #snakemake --rulegraph --forceall -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" | dot -Tpdf > rulegraph.pdf
+
+
+# DENBI
+snakemake --keep-going --cores 8 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/test_denbi_benchmark.yaml"
