9766d132779ec241b81be04f76ad30bc05db6f16
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-17 12:33:19 +0100

    add denbi test config

---
+++ examples/tasks/task_blood2_small.py
@@ -0,0 +1,52 @@
+from torchvision import transforms
+from domainlab.tasks.task_folder_mk import mk_task_folder
+from domainlab.tasks.utils_task import ImSize
+
+
+img_size = 36
+# img_size = 28
+
+trans = transforms.Compose([
+    transforms.Resize((img_size, img_size)),
+    transforms.RandomResizedCrop(img_size, scale=(0.7, 1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(0.3, 0.3, 0.3, 0.3),
+    transforms.RandomGrayscale(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+trans_te = transforms.Compose([
+    transforms.Resize((img_size, img_size)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406],
+                         [0.229, 0.224, 0.225])
+])
+
+
+task = mk_task_folder(extensions={"acevedo": "jpg", "matek": "tiff", "mll": "tif"},
+                      list_str_y=["monocyte", "eosinophil"],
+                      dict_domain_folder_name2class={
+                          "acevedo": {"monocyte": "monocyte",
+                                  "eosinophil": "eosinophil"},
+                          "matek": {"eosinophil": "eosinophil",
+                                       "monocyte": "monocyte"},
+                          "mll": {"monocyte": "monocyte",
+                                  "eosinophil": "eosinophil"}},
+                      dict_domain_img_trans={
+                          "acevedo": trans,
+                          "mll": trans,
+                          "matek": trans,
+                      },
+                      img_trans_te=trans_te,
+                      isize=ImSize(3, img_size, img_size),
+                      dict_domain2imgroot={
+                          "matek": "/home/ubuntu/_Domains/Matek/",
+                          "mll": "/home/ubuntu/_Domains/MLL/",
+                          "acevedo": "/home/ubuntu/_Domains/Acevedo/"},
+                      taskna="blood_mon_eos_bas")
+
+
+def get_task(na=None):
+    return task
