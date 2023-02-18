0b40093f9bae20b34518c3043bb93985085b25f0
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-18 19:06:26 +0100

    denbi dset sanity check

---
+++ examples/benchmark/denbi_dset_sanity_check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python main_out.py --te_d=matek --tpath=examples/tasks/task_blood2_small.py --debug --bs=2 --aname=diva --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 --gamma_y=7e5 --gamma_d=1e5 --san_check --san_num=4
