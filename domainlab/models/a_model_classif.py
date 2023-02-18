2a95b9f3694c5684ea71e2cef162f064ef3195a2
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-18 20:45:13 +0100

    disable confmat prints to reduce log size

--- domainlab/models/a_model_classif.py
+++ domainlab/models/a_model_classif.py
@@ -37,19 +37,19 @@ class AModelClassif(AModel, metaclass=abc.ABCMeta):
         metric_te = None
         metric_tr_pool = self.perf_metric.cal_metrics(self, loader_tr, device)
         confmat = metric_tr_pool.pop("confmat")
-        print("pooled train domains performance:")
-        rprint(metric_tr_pool)
-        print("confusion matrix:")
-        print(pd.DataFrame(confmat))
+        # print("pooled train domains performance:")
+        # rprint(metric_tr_pool)
+        # print("confusion matrix:")
+        # print(pd.DataFrame(confmat))
         metric_tr_pool["confmat"] = confmat
         # test set has no domain label, so can be more custom
         if loader_te is not None:
             metric_te = self.perf_metric.cal_metrics(self, loader_te, device)
             confmat = metric_te.pop("confmat")
-            print("out of domain test performance:")
-            rprint(metric_te)
-            print("confusion matrix:")
-            print(pd.DataFrame(confmat))
+            # print("out of domain test performance:")
+            # rprint(metric_te)
+            # print("confusion matrix:")
+            # print(pd.DataFrame(confmat))
             metric_te["confmat"] = confmat
         return metric_te
 
