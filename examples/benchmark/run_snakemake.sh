05b7dc9ee2af254ad8414ac37837bcb2b965dd41
schoersch <14273062+schoersch@users.noreply.github.com>
2023-02-23 09:05:06 +0100

    4 cores

--- examples/benchmark/run_snakemake.sh
+++ examples/benchmark/run_snakemake.sh
@@ -35,5 +35,5 @@ echo "verbose log: $logfile"
 
 
 # DENBI MNIST
-snakemake --keep-going --keep-incomplete --notemp --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/denbi_mnist.yaml" 2>&1 | tee $logfile
+snakemake --keep-going --keep-incomplete --notemp --cores 4 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/denbi_mnist.yaml" 2>&1 | tee $logfile
 
