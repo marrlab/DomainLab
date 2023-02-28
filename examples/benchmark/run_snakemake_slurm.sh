0b9154c70c927524fa3c13b5a0a8f85a2e696ca0
smilesun <smilesun.east@gmail.com>
2023-02-28 13:54:44 +0100

    working with run_snamkemake_slurm.sh

--- examples/benchmark/run_snakemake_slurm.sh
+++ examples/benchmark/run_snakemake_slurm.sh
@@ -5,37 +5,11 @@ timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
 
-
+export DOMAINLAB_CUDA_START_SEED=$1
 logdir="zoutput/logs"
 mkdir -p $logdir
 logfile="$logdir/$(timestamp).out"
 echo "verbose log: $logfile"
 
-# -n: dry-run  (A dry run is a software testing process where the effects of a possible failure are intentionally mitigated, For example, there is rsync utility for transfer data over some interface, but user can try rsync with dry-run option to check syntax and test communication without data transferring.)
-# -p: print shell commands
-# -d: specify working directory. This should be the DomainLab dir
-# -s: snakefile
-# -- configfile: configuration yaml file of the benchmark
-
-# DENBI
-#snakemake --keep-going --keep-incomplete --notemp --cores 5 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/helm_runtime_evaluation.yaml" 2>&1 | tee $logfile
-# TODO create a snakemake call (alike above) to run on the helmholz vm
-# TODO set a reasonable number of cores (cores = 5 was very usefull, as we start 5 algorithms -> each algo gets one core)
-
 # Helmholtz
-snakemake --profile "/home/icb/xinyue.zhang/DomainLab/examples/yaml/slurm" --keep-going --keep-incomplete --notemp --cores 5 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/test_helm_benchmark.yaml" 2>&1 | tee $logfile 
-=======
-#snakemake -np -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
-
-snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" --keep-going --summary  # this will give us a clue first what jobs will be run
-
-snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" --keep-going
-
-#snakemake --rerun-incomplete --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
-
-
-# print execution graph to pdf
-
-#snakemake --dag --forceall -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" | dot -Tpdf > dag.pdf
-
-#snakemake --rulegraph --forceall -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" | dot -Tpdf > rulegraph.pdf
+snakemake --profile "examples/yaml/slurm" --keep-going --keep-incomplete --notemp --cores 5 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/test_helm_benchmark.yaml" 2>&1 | tee $logfile 
