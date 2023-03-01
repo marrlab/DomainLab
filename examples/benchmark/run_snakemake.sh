e588e73f7048578e5c00242c5f6f7c4ff2745d2e
smilesun <smilesun.east@gmail.com>
2023-03-01 13:48:04 +0100

    only plot rule in script

--- examples/benchmark/run_snakemake.sh
+++ examples/benchmark/run_snakemake.sh
@@ -8,12 +8,16 @@
 #snakemake -np -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 export DOMAINLAB_CUDA_START_SEED=$1
 
+# first display all tasks
 snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" --keep-going --summary  # this will give us a clue first what jobs will be run
 
+# second submit the jobs
 snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" --keep-going
 
 #snakemake --rerun-incomplete --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml"
 
+# only plot
+snakemake --cores 1 -s "domainlab/exp_protocol/benchmark.smk" --configfile "examples/yaml/demo_benchmark.yaml" --keep-going --allowed-rules gen_plots
 
 # print execution graph to pdf
 
