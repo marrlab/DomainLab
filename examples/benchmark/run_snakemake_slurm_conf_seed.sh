2ed0e7bd7dac78681a60d7920a5d1942df649684
smilesun <smilesun.east@gmail.com>
2023-03-03 15:22:53 +0100

    .

--- examples/benchmark/run_snakemake_slurm_conf_seed.sh
+++ examples/benchmark/run_snakemake_slurm_conf_seed.sh
@@ -12,6 +12,7 @@ logdir="zoutput/logs"
 mkdir -p $logdir
 logfile="$logdir/$(timestamp).out"
 echo "Configuration file: $CONFIGFILE"
+echo "starting seed is: $DOMAINLAB_CUDA_START_SEED"
 echo "verbose log: $logfile"
 # Helmholtz
 snakemake --profile "examples/yaml/slurm" --keep-going --keep-incomplete --notemp --cores 5 -s "domainlab/exp_protocol/benchmark.smk" --configfile $CONFIGFILE 2>&1 | tee $logfile 
