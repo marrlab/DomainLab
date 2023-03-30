e06499c80044cd880c843f9e3a9863421c69f0cd
smilesun <smilesun.east@gmail.com>
2023-03-30 15:15:31 +0200

    dim mismatch

--- domainlab/algos/builder_match_hduva.py
+++ domainlab/algos/builder_match_hduva.py
@@ -5,8 +5,6 @@ from domainlab.algos.a_algo_builder import NodeAlgoBuilder
 from domainlab.algos.msels.c_msel import MSelTrLoss
 from domainlab.algos.msels.c_msel_oracle import MSelOracleVisitor
 from domainlab.algos.observers.b_obvisitor import ObVisitor
-from domainlab.algos.observers.c_obvisitor_cleanup import ObVisitorCleanUp
-from domainlab.algos.trainers.train_visitor import TrainerVisitor
 from domainlab.tasks.utils_task_dset import DsetIndDecorator4XYD
 from domainlab.compos.pcr.request import RequestVAEBuilderCHW
 from domainlab.compos.vae.utils_request_chain_builder import VAEChainNodeGetter
@@ -14,8 +12,7 @@ from domainlab.models.model_hduva import mk_hduva
 from domainlab.utils.utils_cuda import get_device
 
 from domainlab.algos.trainers.train_matchdg import TrainerMatchDG
-from domainlab.models.wrapper_matchdg import ModelWrapMatchDGLogit
-from domainlab.models.model_wrapper_matchdg4net import ModelWrapMatchDGNet
+from domainlab.models.model_wrapper_matchdg4vae import ModelWrapMatchDGVAE
 
 
 class NodeAlgoBuilderMatchHDUVA(NodeAlgoBuilder):
@@ -51,10 +48,25 @@ class NodeAlgoBuilderMatchHDUVA(NodeAlgoBuilder):
                            beta_y=args.beta_y,
                            beta_d=args.beta_d)
 
-        model = ModelWrapMatchDGLogit(model, list_str_y=task.list_str_y)
+        model_ctr = mk_hduva()(node,
+                               zd_dim=args.zd_dim,
+                               zy_dim=args.zy_dim,
+                               zx_dim=args.zx_dim,
+                               device=device,
+                               topic_dim=args.topic_dim,
+                               list_str_y=task.list_str_y,
+                               list_d_tr=task.list_domain_tr,
+                               gamma_d=args.gamma_d,
+                               gamma_y=args.gamma_y,
+                               beta_t=args.beta_t,
+                               beta_x=args.beta_x,
+                               beta_y=args.beta_y,
+                               beta_d=args.beta_d)
+
+        model = ModelWrapMatchDGVAE(model, list_str_y=task.list_str_y)
         model = model.to(device)
 
-        ctr_model = ModelWrapMatchDGNet(model, list_str_y=task.list_str_y)
+        ctr_model = ModelWrapMatchDGVAE(model_ctr, list_str_y=task.list_str_y)
         ctr_model = ctr_model.to(device)
 
         model_sel = MSelOracleVisitor(MSelTrLoss(max_es=args.es))
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=2 --aname=matchdg --epochs_ctr=3 --epochs_erm=3 --npath=examples/nets/resnet.py
 
-python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=2 --aname=matchhduva --epochs_ctr=3 --epochs_erm=3 --npath=examples/nets/resnet.py --nname=alexnet --gamma_y=7e5 --nname_topic_distrib_img2topic=conv_bn_pool_2 --nname_encoder_sandwich_layer_img2h4zd=alexnet
+python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=2 --aname=matchhduva --epochs_ctr=3 --epochs_erm=3 --nname=alexnet --gamma_y=7e5 --nname_topic_distrib_img2topic=conv_bn_pool_2 --nname_encoder_sandwich_layer_img2h4zd=alexnet
