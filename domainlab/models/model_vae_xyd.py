85bf0257e907899c648e4bc139b9868cb202ed17
smilesun <smilesun.east@gmail.com>
2023-03-31 16:46:07 +0200

    .

--- domainlab/models/model_vae_xyd.py
+++ domainlab/models/model_vae_xyd.py
@@ -4,6 +4,8 @@ Base Class for XYD VAE
 import torch
 import torch.distributions as dist
 
+from domainlab.utils.utils_class import store_args
+from domainlab.models.a_model_classif import AModelClassif
 from domainlab.models.a_model import AModel
 
 
@@ -11,19 +13,18 @@ class VAEXYD(AModel):
     """
     Base Class for XYD VAE
     """
+    @store_args
     def __init__(self, chain_node_builder,
                  zd_dim, zy_dim, zx_dim,
-                 list_str_d):
+                 list_str_y=None,
+                 list_str_d=None):
         """
         :param chain_node_builder: constructed object
         """
         super().__init__()
-        self.list_str_d = list_str_d
-        self.chain_node_builder = chain_node_builder
-        self.zd_dim = zd_dim
-        self.zy_dim = zy_dim
-        self.zx_dim = zx_dim
+        self.init()
 
+    def init(self):
         self.chain_node_builder.init_business(
             self.zd_dim, self.zx_dim, self.zy_dim)
         self.i_c = self.chain_node_builder.i_c
@@ -42,9 +43,6 @@ class VAEXYD(AModel):
         self.add_module("net_p_zy",
                         self.chain_node_builder.construct_cond_prior(
                             self.dim_y, self.zy_dim))
-        self.add_module("net_classif_y",
-                        self.chain_node_builder.construct_classifier(
-                            self.zy_dim, self.dim_y))
 
     def init_p_zx4batch(self, batch_size, device):
         """
