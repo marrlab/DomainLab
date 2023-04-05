64984d6efd99793dda40bd6e4c8c8bc0cdbf592f
smilesun <smilesun.east@gmail.com>
2023-04-05 19:11:50 +0200

    doc to doc2

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@ set -e  # exit upon first error
 starttime=`date +%s`
 bash -x -v ci_run_examples.sh
 sh ci_pytest_cov.sh
-git checkout doc
+git checkout doc2
 git merge master
 sh gen_doc.sh
 endtime=`date +%s`
