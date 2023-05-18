f7afc06095ad46b7bdca70da66357e7026ce5d42
carla <carla.feistner@tum.de>
2023-05-18 16:17:16 +0200

    doc

--- domainlab/utils/logger.py
+++ domainlab/utils/logger.py
@@ -1,12 +1,25 @@
+'''
+A logger for our software
+'''
 import os
 import logging
 
 
 class Logger:
+    '''
+    static logger class
+    '''
     logger = None
 
     @staticmethod
     def get_logger(logger_name='logger', loglevel='INFO'):
+        '''
+        returns a logger
+        if no logger was created yet, it will create a logger with the name
+        specified in logger_name with the level specified in loglevel.
+        If the logger was created for the first time the arguments do not change
+        anything at the behaviour anymore
+        '''
         if Logger.logger is None:
             Logger.logger = logging.getLogger(logger_name)
             Logger.logger.setLevel(loglevel)
