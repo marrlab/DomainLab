00932288ff76a4cc0614f1264697da58c1fc485d
smilesun <smilesun.east@gmail.com>
2023-06-13 15:14:50 +0200

    cpu to generate adversarial image

--- domainlab/algos/trainers/train_dial.py
+++ domainlab/algos/trainers/train_dial.py
@@ -44,7 +44,8 @@ class TrainerDIAL(TrainerBasic):
                 tensor_x.to(self.device), vec_y.to(self.device), vec_d.to(self.device)
             self.optimizer.zero_grad()
             loss = self.model.cal_loss(tensor_x, vec_y, vec_d)  # @FIXME
-            tensor_x_adv = self.gen_adversarial(self.device, tensor_x, vec_y)
+            # tensor_x_adv = self.gen_adversarial(self.device, tensor_x, vec_y)
+            tensor_x_adv = self.gen_adversarial(torch.device("cpu"), tensor_x, vec_y)
             tensor_x_batch_adv_no_grad = Variable(tensor_x_adv, requires_grad=False)
             loss_dial = self.model.cal_loss(tensor_x_batch_adv_no_grad, vec_y, vec_d)  # @FIXME
             loss = loss.sum() + self.aconf.gamma_reg * loss_dial.sum()
