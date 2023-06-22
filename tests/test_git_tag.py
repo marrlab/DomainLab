a4920094d67774ed35b53222446c3312c1126a8f
carla <carla.feistner@tum.de>
2023-06-22 13:41:15 +0200

    use a dummy file

--- tests/test_git_tag.py
+++ tests/test_git_tag.py
@@ -4,6 +4,11 @@ Code coverage issues:
     - lines 10-20
     - lines 28, 30-32
 '''
+import os
+
+import pytest
+from subprocess import CalledProcessError
+
 from domainlab.utils.get_git_tag import get_git_tag
 
 def test_git_tag():
@@ -11,3 +16,23 @@ def test_git_tag():
     test git_tag
     """
     get_git_tag(print_diff=True)
+
+def test_git_tag_error():
+    '''
+    test git_tag error
+    '''
+    # add one line to the file
+    with open("data/ztest_files/dummy_file.py", "a") as f:
+        f.write("\n# I am a dummy command")
+    get_git_tag(print_diff=True)
+    # delete the last line on the file again
+    with open("data/ztest_files/dummy_file.py", "r") as f:
+        lines = f.readlines()
+        lines = lines[:-1]
+    #os.remove("main_out.py")
+    with open("data/ztest_files/dummy_file.py", "w") as f:
+        for num, line in enumerate(lines):
+            if num == len(lines) - 1:
+                f.write(line[:-2])
+            else:
+                f.write(line)
