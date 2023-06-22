0642c79ce4700a2dc0e035c5a05d99e3df829324
carla <carla.feistner@tum.de>
2023-06-22 15:38:47 +0200

    add md files with code examples to ci_run_examples.sh

--- ci_run_examples.sh
+++ ci_run_examples.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x -v
 set -e  # exit upon first error
-sed 's/`//g' docs/doc_examples.md > ./sh_temp.sh
+sed 's/`//g' docs/doc_examples.md >> ./sh_temp.sh
+sed 's/`//g' docs/doc_MNIST_classification.md >> ./sh_temp.sh
+sed 's/`//g' docs/doc_benchmark.md >> ./sh_temp.sh
+sed 's/`//g' docs/doc_custom_nn.md >> ./sh_temp.sh
+sed 's/`//g' docs/doc_extend_contribute.md >> ./sh_temp.sh
+sed 's/`//g' docs/doc_tasks.md >> ./sh_temp.sh
+sed 's/`//g' README.md >> ./sh_temp.sh
 bash -x -v -e sh_temp.sh
