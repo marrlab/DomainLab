14e88fda0d813aecd04eabfc4b451ee84d442278
smilesun <smilesun.east@gmail.com>
2023-07-04 15:41:41 +0200

    .

--- ci_run_examples.sh
+++ ci_run_examples.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x -v
 set -e  # exit upon first error
-sed -n '/```shell/,/```/ p' docs/doc_examples.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' docs/doc_MNIST_classification.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' docs/doc_benchmark.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' docs/doc_custom_nn.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' docs/doc_extend_contribute.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' docs/doc_tasks.md | sed '/^```/ d' >> ./sh_temp.sh
-sed -n '/```shell/,/```/ p' README.md | sed '/^```/ d' >> ./sh_temp.sh
-bash -x -v -e sh_temp.sh
+# >> append content
+# > erase original content
+sed -n '/```shell/,/```/ p' docs/doc_examples.md | sed '/^```/ d' >> ./sh_temp_example.sh
+bash -x -v -e sh_temp_example.sh
+
+sed -n '/```shell/,/```/ p' docs/doc_MNIST_classification.md | sed '/^```/ d' > ./sh_temp_mnist.sh
+bash -x -v -e sh_temp_mnist.sh
+
+# sed -n '/```shell/,/```/ p' docs/doc_benchmark.md | sed '/^```/ d' >> ./sh_temp.sh
+sed -n '/```shell/,/```/ p' docs/doc_custom_nn.md | sed '/^```/ d' > ./sh_temp.sh
+sed -n '/```shell/,/```/ p' docs/doc_extend_contribute.md | sed '/^```/ d' > ./sh_temp.sh
+sed -n '/```shell/,/```/ p' docs/doc_tasks.md | sed '/^```/ d' > ./sh_temp.sh
+sed -n '/```shell/,/```/ p' README.md | sed '/^```/ d' > ./sh_temp_readme.sh
+bash -x -v -e sh_temp_readme.sh
