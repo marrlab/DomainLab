a62de39ad9c468720ac9dd6a043bc0523e208f10
smilesun <smilesun.east@gmail.com>
2023-08-04 15:28:42 +0200

    almost done

---
+++ domainlab/algos/builder_api_model.py
@@ -0,0 +1,32 @@
+"""
+build algorithm from API coded model with custom backbone
+"""
+from domainlab.algos.a_algo_builder import NodeAlgoBuilder
+from domainlab.algos.msels.c_msel_val import MSelValPerf
+from domainlab.algos.msels.c_msel_oracle import MSelOracleVisitor
+from domainlab.algos.observers.b_obvisitor import ObVisitor
+from domainlab.algos.observers.c_obvisitor_cleanup import ObVisitorCleanUp
+from domainlab.algos.trainers.zoo_trainer import TrainerChainNodeGetter
+from domainlab.compos.pcr.request import RequestVAEBuilderCHW
+from domainlab.compos.vae.utils_request_chain_builder import VAEChainNodeGetter
+from domainlab.models.model_hduva import mk_hduva
+from domainlab.utils.utils_cuda import get_device
+
+
+class NodeAlgoBuilderAPIModel(NodeAlgoBuilder):
+    """
+    build algorithm from API coded model with custom backbone
+    """
+    def init_business(self, exp):
+        """
+        return trainer, model, observer
+        """
+        task = exp.task
+        args = exp.args
+        device = get_device(args)
+        model_sel = MSelValPerf(max_es=args.es)
+        observer = ObVisitorCleanUp(
+            ObVisitor(exp, model_sel, device))
+        trainer = TrainerChainNodeGetter(args)(default="visitor")
+        trainer.init_business(exp.model, task, observer, device, args)
+        return trainer
--- mk_exp.py
+++ mk_exp.py
@@ -4,37 +4,55 @@ make an experiment
 from domainlab.arg_parser import mk_parser_main
 from domainlab.compos.exp.exp_main import Exp
 
-def mk_exp(task, test_domain, batchsize):
-    str_arg = f"--te_d={test_domain} --bs={batchsize}"
+def mk_exp(task, model, trainer, test_domain, batchsize):
+    str_arg = f"--task=mnist10 --aname=deepall --trainer={trainer} --te_d={test_domain} --bs={batchsize} --max_es=1"
     parser = mk_parser_main()
     conf = parser.parse_args(str_arg.split())
-    exp = Exp(conf, task)
+    exp = Exp(conf, task)   # FIXME: trainer does not need to be executed twice
+    # trainer = TrainerMLDG()
+    exp.trainer.init_business(model, task, exp.trainer.observer, device, conf)
     return exp
 
 
 
 def test():
-
+    from torch import nn
     from torchvision import models as torchvisionmodels
+    from torchvision import transforms
     from torchvision.models import ResNet50_Weights
-    from domainlab.tasks.dset import mk_task_dset
+
+    from domainlab.dsets.dset_mnist_color_solo_default import DsetMNISTColorSoloDefault
+    from domainlab.tasks.task_dset import mk_task_dset
     from domainlab.models.model_deep_all import mk_deepall
-    from domainlab.algos.trainers.trainer_mldg import TrainerMLDG
+    from domainlab.tasks.utils_task import ImSize
 
-    from domainlab.compos.nn_zoo.nn import LayerId
-    from domainlab.compos.nn_zoo.nn_torchvision import NetTorchVisionBase
+    # specify domain generalization task
+    DICT_DOMAIN2DSET = {}  # build dictionary of domains, here we give domains named "d1", "d2", "d3".
+    DICT_DOMAIN2DSET["d1"] = (DsetMNISTColorSoloDefault(0, "zout"),
+            DsetMNISTColorSoloDefault(0, "zout"))  # first position in tuple is training, second is validation
+    DICT_DOMAIN2DSET["d2"] = (DsetMNISTColorSoloDefault(1, "zout"),  # train and validation for domain "d2"
+            DsetMNISTColorSoloDefault(1, "zout"))
+    DICT_DOMAIN2DSET["d3"] = (DsetMNISTColorSoloDefault(2, "zout"), 
+            DsetMNISTColorSoloDefault(2, "zout"))  # train and validation for domain "d3"
 
-    task = mk_task_dset
-    exp = mk_exp(task, test_domain=["0", "1", "2"], batchsize=32)
-    # model_sel = MSelValPerf(max_es=1)
-    # observer = ObVisitor(exp, model_sel, device=torch.device("gpu"))
-    # code backbone
+    IMG_TRANS = transforms.Compose([transforms.ToTensor()])      # specify transformations to use for training data
+
+    task = mk_task_dset(DICT_DOMAIN2DSET,  # the constructed dictionary above
+                        list_str_y=[str(ele) for ele in range(0, 10)],  # a list of strings naming each class to classify
+                        isize=ImSize(3, 28, 28),   # the image size
+                        dict_domain_img_trans={"d1": IMG_TRANS, "d2": IMG_TRANS, "d3": IMG_TRANS},  # for each domain, one could specify different transformations
+                        img_trans_te=IMG_TRANS, # use the same transformation for test set
+                        taskna="demo_task")  # give a name for your task constructed.
+
+    # specify backbone to use
     dim_y = 10
     backbone = torchvisionmodels.resnet.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     num_final_in = backbone.fc.in_features
     backbone.fc = nn.Linear(num_final_in, dim_y)
-    ## make model
+
+    ## specify model to use
     model = mk_deepall()(backbone)
+
     ## make trainer for model
-    trainer = TrainerMLDG()
-    trainer.init_business(model, task, observer, device, args)
+    exp = mk_exp(task, model, trainer="mldg", test_domain=["0", "1", "2"], batchsize=32)
+    exp.execute()
