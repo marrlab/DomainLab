ecf5348d1d9622d606efbc161e38a042589b49ad
smilesun <smilesun.east@gmail.com>
2023-08-04 14:48:00 +0200

    .

--- mk_exp.py
+++ mk_exp.py
@@ -4,8 +4,9 @@ make an experiment
 from domainlab.arg_parser import mk_parser_main
 from domainlab.compos.exp.exp_main import Exp
 
-def mk_exp(task):
+def mk_exp(task, test_domain, batchsize):
+    str_arg = f"--te_d={test_domain} --bs={batchsize}"
     parser = mk_parser_main()
-    conf = parser.parse_args(str)
+    conf = parser.parse_args(str_arg.split())
     exp = Exp(conf, task)
     return exp
