5d8e9701a2678c85c737d2bfe4c9fb86185c6004
smilesun <smilesun.east@gmail.com>
2023-08-04 19:33:56 +0200

    remove unused imports

--- domainlab/algos/builder_api_model.py
+++ domainlab/algos/builder_api_model.py
@@ -2,11 +2,7 @@
 build algorithm from API coded model with custom backbone
 """
 from domainlab.algos.a_algo_builder import NodeAlgoBuilder
-from domainlab.algos.msels.c_msel_val import MSelValPerf
-from domainlab.algos.observers.b_obvisitor import ObVisitor
-from domainlab.algos.observers.c_obvisitor_cleanup import ObVisitorCleanUp
 from domainlab.algos.trainers.zoo_trainer import TrainerChainNodeGetter
-from domainlab.utils.utils_cuda import get_device
 
 
 class NodeAlgoBuilderAPIModel(NodeAlgoBuilder):
