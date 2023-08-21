3f6904ebf3e0fdf7a0d03152a9088e43387b3054
smilesun <smilesun.east@gmail.com>
2023-08-21 17:14:49 +0200

    .

--- examples/tasks/task_dset_custom.py
+++ examples/tasks/task_dset_custom.py
@@ -4,10 +4,11 @@ example task construction:
 """
 from domainlab.tasks.task_dset import mk_task_dset
 from domainlab.tasks.utils_task import ImSize
-from domainlab.dsets.dset_mnist_color_solo_default import DsetMNISTColorSoloDefault
+from domainlab.dsets.dset_mnist_color_solo_default import \
+    DsetMNISTColorSoloDefault
 
 
-task = mk_task_dset(isize=ImSize(3, 28, 28),  dim_y=10, taskna="custom_task")
+task = mk_task_dset(isize=ImSize(3, 28, 28), dim_y=10, taskna="custom_task")
 task.add_domain(name="domain1",
                 dset_tr=DsetMNISTColorSoloDefault(0),
                 dset_val=DsetMNISTColorSoloDefault(1))
