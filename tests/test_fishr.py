79691b8ec98056f6238f67ad4ba30acee169ea8e
smilesun <smilesun.east@gmail.com>
2023-08-23 10:10:52 +0200

    .

--- tests/test_fishr.py
+++ tests/test_fishr.py
@@ -8,5 +8,5 @@ def test_deepall_mldg():
     """
     train DeepAll with MLDG
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=8 --aname=deepall --trainer=fishr --nname=alexnet"
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --aname=deepall --trainer=fishr --nname=alexnet"
     utils_test_algo(args)
