1cb8eafcecde26fabb38e119e7a361c3c1e864ef
lisab00 <lisa.beer@tum.de>
2023-09-01 10:36:07 +0200

    test mk epx diva

--- tests/test_mk_exp_diva.py
+++ tests/test_mk_exp_diva.py
@@ -1,6 +1,9 @@
 """
 make an experiment using "diva" model
 """
+from torch import nn
+from torchvision import models as torchvisionmodels
+from torchvision.models import ResNet50_Weights
 
 from domainlab.mk_exp import mk_exp
 from domainlab.dsets.dset_mnist_color_solo_default import DsetMNISTColorSoloDefault
@@ -8,54 +11,61 @@ from domainlab.tasks.task_dset import mk_task_dset
 from domainlab.models.model_diva import mk_diva
 from domainlab.tasks.utils_task import ImSize
 from domainlab.compos.vae.utils_request_chain_builder import VAEChainNodeGetter
-from domainlab.compos.pcr.request import RequestVAEBuilderCHW
+from domainlab.compos.pcr.request import RequestVAEBuilderNN
 
 
-def test_mk_exp_diva():
-    """
-    test mk experiment API for "diva" model and trainers "mldg", "dial"
-    """
+#def test_mk_exp_diva():
+"""
+test mk experiment API for "diva" model and trainers "mldg", "dial"
+"""
 
-    mk_exp_diva(trainer="mldg")
-    mk_exp_diva(trainer="dial")
+ #   mk_exp_diva(trainer="mldg")
+  #  mk_exp_diva(trainer="dial")
 
 
-def mk_exp_diva(trainer="mldg"):
-    """
-    execute experiment with "diva" model and custom trainer
-    """
+# def mk_exp_diva(trainer="mldg"):
+"""
+execute experiment with "diva" model and custom trainer
+"""
 
-    # specify domain generalization task
-    task = mk_task_dset(dim_y=10, isize=ImSize(3, 28, 28),  taskna="custom_task")
-    task.add_domain(name="domain1",
-                dset_tr=DsetMNISTColorSoloDefault(0),
-                dset_val=DsetMNISTColorSoloDefault(1))
-    task.add_domain(name="domain2",
-                dset_tr=DsetMNISTColorSoloDefault(2),
-                dset_val=DsetMNISTColorSoloDefault(3))
-    task.add_domain(name="domain3",
-                dset_tr=DsetMNISTColorSoloDefault(4),
-                dset_val=DsetMNISTColorSoloDefault(5))
+# specify domain generalization task
+task = mk_task_dset(dim_y=10, isize=ImSize(3, 28, 28),  taskna="custom_task")
+task.add_domain(name="domain1",
+            dset_tr=DsetMNISTColorSoloDefault(0),
+            dset_val=DsetMNISTColorSoloDefault(1))
+task.add_domain(name="domain2",
+            dset_tr=DsetMNISTColorSoloDefault(2),
+            dset_val=DsetMNISTColorSoloDefault(3))
+task.add_domain(name="domain3",
+            dset_tr=DsetMNISTColorSoloDefault(4),
+            dset_val=DsetMNISTColorSoloDefault(5))
 
-    # specify parameters
-    zd_dim = 3
-    zy_dim = 10
-    zx_dim = 30
-    args = f"--aname=apimodel --trainer={trainer} --te_d=domain1 --bs=32"
-    request = RequestVAEBuilderCHW(task.isize.c, task.isize.h, task.isize.w, args)
-    chain_node_builder = VAEChainNodeGetter(request)()  # TODO: chain_node_builder
-    list_str_y = [f"class{i}" for i in range(task.dim_y)]
-    list_d_tr = ["domain2", "domain3"]
-    gamma_d = 1e5
-    gamma_y = 7e5
-    beta_d = 1e3
-    beta_x = 1e3
-    beta_y = 1e3
+#remove me
+trainer = "mldg"
 
-    # specify model to use
-    model = mk_diva()(chain_node_builder, zd_dim, zy_dim, zx_dim, list_str_y, list_d_tr, gamma_d,
-                      gamma_y, beta_d, beta_x, beta_y)
+# specify parameters
+num_dom = 3
+zd_dim = 3
+zy_dim = 10
+zx_dim = 30
+net_class_d = nn.Linear( 6 , num_dom)
+net_x = torchvisionmodels.resnet.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+net_class_y = nn.Linear( 6, task.dim_y)
+request = RequestVAEBuilderNN(task.isize.c, task.isize.h, task.isize.w,
+                              net_class_d, net_x, net_class_y)
+chain_node_builder = VAEChainNodeGetter(request)()  # TODO: chain_node_builder
+list_str_y = [f"class{i}" for i in range(task.dim_y)]
+list_d_tr = ["domain2", "domain3"]
+gamma_d = 1e5
+gamma_y = 7e5
+beta_d = 1e3
+beta_x = 1e3
+beta_y = 1e3
 
-    # make trainer for model
-    exp = mk_exp(task, model, trainer=trainer, test_domain="domain1", batchsize=32)
-    exp.execute(num_epochs=2)
+# specify model to use
+model = mk_diva()(chain_node_builder, zd_dim, zy_dim, zx_dim, list_str_y, list_d_tr, gamma_d,
+                  gamma_y, beta_d, beta_x, beta_y)
+
+# make trainer for model
+exp = mk_exp(task, model, trainer=trainer, test_domain="domain1", batchsize=32)
+exp.execute(num_epochs=2)
