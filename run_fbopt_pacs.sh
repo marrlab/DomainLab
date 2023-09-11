867b00668d4be0c900f13c8946d2099b0f8bb8ed
smilesun <smilesun.east@gmail.com>
2023-09-11 16:56:18 +0200

    .

--- run_fbopt_pacs.sh
+++ run_fbopt_pacs.sh
@@ -3,4 +3,5 @@
 # although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
 # so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
 # pytest -s tests/test_fbopt.py
-python main_out.py --te_d=sketch --tpath=examples/tasks/task_pacs_path_list.py --bs=4 --aname=dann --trainer=fbopt --nname=alexnet --epos=20
+# python main_out.py --te_d=sketch --tpath=examples/tasks/task_pacs_path_list.py --bs=2 --aname=dann --trainer=fbopt --nname=alexnet --epos=20
+python main_out.py --te_d=sketch --tpath=examples/tasks/demo_task_path_list_small.py --bs=16 --aname=dann --trainer=fbopt --nname=alexnet --epos=20
