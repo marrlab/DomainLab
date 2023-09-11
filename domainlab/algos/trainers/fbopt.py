086348125cf0d396cda329e8accfec38dd640f6d
smilesun <smilesun.east@gmail.com>
2023-09-11 14:07:37 +0200

    .

--- domainlab/algos/trainers/fbopt.py
+++ domainlab/algos/trainers/fbopt.py
@@ -1,6 +1,7 @@
 """
 update hyper-parameters during training
 """
+import copy
 
 
 class HyperSchedulerFeedback():
@@ -55,15 +56,18 @@ class HyperSchedulerFeedback():
         flag_success = False
         self.ploss_old_theta_new_mu = self.trainer.eval_loss(mmu_new, self.dict_theta)
         self.ploss_old_theta_old_mu = self.trainer.eval_loss(self.mmu, self.dict_theta)
+        theta4mu_new = copy.deepcopy(self.dict_theta)
         for i in range(self.budget_theta_update_per_mu):
             print(f"update theta at iteration {i} with mu={mmu_new}")
-            theta4mu_new = self.trainer.opt_theta(mmu_new, self.dict_theta)
+            theta4mu_new = self.trainer.opt_theta(mmu_new, theta4mu_new)
             self.ploss_new_theta_new_mu = self.trainer.eval_loss(mmu_new, theta4mu_new)
             self.ploss_new_theta_old_mu = self.trainer.eval_loss(self.mmu, theta4mu_new)
-            self.dict_theta = theta4mu_new
             if self.is_criteria_met():
                 self.mmu = mmu_new
                 flag_success = True
+                # FIXME: update theta only if current mu is good enough?
+                self.dict_theta = theta4mu_new
+                return flag_success
         return flag_success
 
     def inner_product(self, mmu, v_reg_loss):
