d8db3fd1d9a8d0d4f0e246bbd03610bc23f8543a
smilesun <smilesun.east@gmail.com>
2023-09-11 15:35:49 +0200

    .

--- run_fbopt.sh
+++ run_fbopt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=""   
+# export CUDA_VISIBLE_DEVICES=""   
 # although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
 # so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
-pytest -s tests/test_fbopt.py
+# pytest -s tests/test_fbopt.py
+python main_out.py --te_d=caltech --task=mini_vlcs --bs=16 --aname=dann --trainer=fbopt --nname=alexnet
