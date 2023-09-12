f1d6fcfbd67b77823e29823146215f5ffe55ae77
smilesun <smilesun.east@gmail.com>
2023-09-12 11:49:42 +0200

    .

--- run_fbopt.sh
+++ run_fbopt.sh
@@ -3,4 +3,4 @@
 # although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
 # so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
 # pytest -s tests/test_fbopt.py
-python main_out.py --te_d=caltech --task=mini_vlcs --bs=16 --aname=dann --trainer=fbopt --nname=alexnet --epos=20
+python main_out.py --te_d=caltech --task=mini_vlcs --bs=16 --aname=dann --trainer=fbopt --nname=alexnet --epos=20 --loglevel=DEBUG
