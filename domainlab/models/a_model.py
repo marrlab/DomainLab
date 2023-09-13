cfaeef62e88e80945d1201a251af39821c278ba1
smilesun <smilesun.east@gmail.com>
2023-09-13 14:43:58 +0200

    .

--- domainlab/models/a_model.py
+++ domainlab/models/a_model.py
@@ -17,6 +17,7 @@ class AModel(nn.Module, metaclass=abc.ABCMeta):
         """
         # FIXME: net1.load_state_dict(net2.state_dict()) contains more information than model.named_parameters() like optimizer status
         # but I dont know another method to set neural network weights without using load_state_dict
+        # FIXME: dict_params lack some keys compared to self.state_dict(), why?
         self.load_state_dict(dict_params, strict=False)
 
     def cal_loss(self, tensor_x, tensor_y, tensor_d=None, others=None):
