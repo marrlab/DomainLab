55678abd3e1593dd9ec7e07cb98de722a8c4fccf
Xudong Sun <smilesun@users.noreply.github.com>
2023-09-22 18:14:58 +0200

    Update train_fbopt.py

--- domainlab/algos/trainers/train_fbopt.py
+++ domainlab/algos/trainers/train_fbopt.py
@@ -132,13 +132,14 @@ class TrainerFbOpt(AbstractTrainer):
         otherwise,
         """
         epo_reg_loss, epo_task_loss = self.eval_r_loss()
-        if self.aconf.msel=="reg":
+        if self.aconf.msel == "tr_loss":
+          if self.aconf.msel_tr_loss =="reg":
             self.epo_loss_tr = epo_reg_loss
-        elif: self.aconf.msel=="erm":
-            self.epo_loss_tr = epo_task_loss
-        elif self.aconf.msel== "tr_loss":
-            epo_p_loss = self.eval_p_loss()
-            self.epo_loss_tr = epo_p_loss
+           if: self.aconf.msel_tr_loss =="erm":
+              self.epo_loss_tr = epo_task_loss
+            elif self.aconf.msel_tr_loss == "p_loss":
+                epo_p_loss = self.eval_p_loss()
+               self.epo_loss_tr = epo_p_loss
         elif self.aconf.msel == "last" or self.aconf.msel == "val":
             self.epo_loss_tr = 1.0 # FIXME: check if this is not used at all
         else:
