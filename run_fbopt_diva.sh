8ed78f89ccaac74e3b547b30e2633af32620068c
smilesun <smilesun.east@gmail.com>
2023-09-23 11:20:32 +0200

    update script

--- run_fbopt_diva.sh
+++ run_fbopt_diva.sh
@@ -3,4 +3,4 @@
 # although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
 # so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
 # pytest -s tests/test_fbopt.py
-python main_out.py --te_d=caltech --task=mini_vlcs --bs=8 --aname=diva --trainer=fbopt --nname=alexnet --nname_dom=alexnet --gamma_d=3  --gamma_y=3 --epos=2000 --beta_mu=3 --budget_theta_update_per_mu=20 --init_mu4beta=0.01 --myoptic_pareto
+python main_out.py --te_d=caltech --task=mini_vlcs --bs=8 --aname=diva --trainer=fbopt --nname=alexnet --nname_dom=alexnet --gamma_d=3  --gamma_y=3 --epos=200
