1921f51721a38d985e31b09943ae769fef3cfdd0
smilesun <smilesun.east@gmail.com>
2023-09-23 20:26:35 +0200

    hduva

--- domainlab/models/model_hduva.py
+++ domainlab/models/model_hduva.py
@@ -38,6 +38,7 @@ def mk_hduva(parent_class=VAEXYDClassif):
             # constructor signature is def __init__(self, **kwargs):
             return functor_scheduler(
                 trainer=trainer,
+                mu_recon=self.multiplier_recon,
                 beta_d=self.beta_d, beta_y=self.beta_y, beta_x=self.beta_x,
                 beta_t=self.beta_t)
 
