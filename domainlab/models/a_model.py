e47e39e580851237d26a2f6a9261b6237b17ca56
smilesun <smilesun.east@gmail.com>
2023-09-28 09:00:18 +0200

    Revert "Merge pull request #311 from marrlab/fishr"
    This reverts commit d7f3c3135c69b068ab640b3295f56adf0f7af962, reversing
    changes made to 174ad27ef7ebb6e034fa5c3ea309455d864fac40.

--- domainlab/models/a_model.py
+++ domainlab/models/a_model.py
@@ -6,13 +6,6 @@ import abc
 
 from torch import nn
 
-try:
-    from backpack import extend
-except:
-    backpack = None
-
-
-
 
 class AModel(nn.Module, metaclass=abc.ABCMeta):
     """
@@ -65,4 +58,3 @@ class AModel(nn.Module, metaclass=abc.ABCMeta):
         :param d:
         """
         return self.cal_loss(tensor_x, tensor_y, tensor_d, others)
-
