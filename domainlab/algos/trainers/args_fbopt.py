2b3cd2a0e4d3fcad54ec325d4b61b75a62566fa4
smilesun <smilesun.east@gmail.com>
2023-10-04 13:33:43 +0200

    init mu as hyperpara

--- domainlab/algos/trainers/args_fbopt.py
+++ domainlab/algos/trainers/args_fbopt.py
@@ -30,9 +30,10 @@ def add_args2parser_fbopt(parser):
     parser.add_argument('--no_setpoint_update', action='store_true', default=False,
                         help='disable setpoint update')
 
-    # the following hyperparamters do not need to be tuned
     parser.add_argument('--init_mu4beta', type=float, default=0.001,
                         help='initial beta for multiplication')
+
+    # the following hyperparamters do not need to be tuned
     parser.add_argument('--beta_mu', type=float, default=1.1,
                         help='how much to multiply mu each time')
     parser.add_argument('--delta_mu', type=float, default=None,
