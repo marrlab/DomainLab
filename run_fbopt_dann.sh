524acd85cc0bac8f047de3b86d30cb0880d37bce
smilesun <smilesun.east@gmail.com>
2023-10-04 15:14:46 +0200

    .

--- run_fbopt_dann.sh
+++ run_fbopt_dann.sh
@@ -1,1 +1,1 @@
-python main_out.py --te_d=caltech --task=mini_vlcs --bs=16 --aname=dann --trainer=fbopt --nname=alexnet --epos=20 --msel=loss_tr --msel_tr_loss=task --no_setpoint_update
+python main_out.py --te_d=caltech --task=mini_vlcs --bs=16 --aname=dann --trainer=fbopt --nname=alexnet --epos=200 --es=200 --no_setpoint_update
