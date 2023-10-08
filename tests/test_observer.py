ed209dd06457b42cb27d3a88f325afe243de074e
Xudong Sun <smilesun@users.noreply.github.com>
2023-10-08 16:57:48 +0200

    Update test_observer.py

--- tests/test_observer.py
+++ tests/test_observer.py
@@ -26,3 +26,23 @@ def test_deepall():
     del exp
     torch.cuda.empty_cache()
     gc.collect()
+
+
+def test_deepall_trloss():
+    """
+    unit deep all
+    """
+    parser = mk_parser_main()
+    margs = parser.parse_args(["--te_d", "caltech",
+                               "--task", "mini_vlcs",
+                               "--aname", "deepall", "--bs", "2",
+                               "--nname", "conv_bn_pool_2",
+                               "--msel", "loss_tr"
+                               ])
+    exp = Exp(margs)
+    exp.trainer.before_tr()
+    exp.trainer.tr_epoch(0)
+    exp.trainer.observer.update(True)
+    del exp
+    torch.cuda.empty_cache()
+    gc.collect()
