35b5ea38962a554ca4bd58ddf8ad5cac955776c8
Xudong Sun <smilesun@users.noreply.github.com>
2023-10-08 16:41:45 +0200

    Create test_observer.py

---
+++ tests/test_observer.py
@@ -0,0 +1,28 @@
+"""
+unit and end-end test for deep all, dann
+"""
+import os
+import gc
+import torch
+from domainlab.compos.exp.exp_main import Exp
+from domainlab.arg_parser import mk_parser_main
+from tests.utils_test import utils_test_algo
+
+
+def test_deepall():
+    """
+    unit deep all
+    """
+    parser = mk_parser_main()
+    margs = parser.parse_args(["--te_d", "caltech",
+                               "--task", "mini_vlcs",
+                               "--aname", "deepall", "--bs", "2",
+                               "--nname", "conv_bn_pool_2"
+                               ])
+    exp = Exp(margs)
+    exp.trainer.before_tr()
+    exp.trainer.tr_epoch(0)
+    exp.trainer.observer.update(True)
+    del exp
+    torch.cuda.empty_cache()
+    gc.collect()
