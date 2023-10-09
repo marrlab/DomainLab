03a67201e29cf85d26a24353b0d2635be907b8d2
Xudong Sun <smilesun@users.noreply.github.com>
2023-10-09 17:55:34 +0200

    Update args_fbopt.py

--- domainlab/algos/trainers/args_fbopt.py
+++ domainlab/algos/trainers/args_fbopt.py
@@ -23,7 +23,7 @@ def add_args2parser_fbopt(parser):
     parser.add_argument('--coeff_ma', type=float, default=0.5,
                         help='exponential moving average')
     
-    parser.add_argument('--coeff_ma_output_state', type=float, default=0.9,
+    parser.add_argument('--coeff_ma_output_state', type=float, default=0.5,
                         help='setpoint output as state exponential moving average')
    
 
