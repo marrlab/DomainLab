874baf14acf3573f2c5b4a80ffa65be95f5982be
smilesun <smilesun.east@gmail.com>
2023-10-10 22:34:30 +0200

    .

---
+++ run_fbopt_mnist_diva.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# export CUDA_VISIBLE_DEVICES=""   
+# although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
+# so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
+# pytest -s tests/test_fbopt.py
+
+python main_out.py --te_d=1 --tr_d 0 3 --task=mnistcolor10 --bs=16 --aname=diva --trainer=fbopt --nname=conv_bn_pool_2 --epos=2000 --es=50 --mu_init=0.00001 --coeff_ma_output_state=0.5 --qaqj
