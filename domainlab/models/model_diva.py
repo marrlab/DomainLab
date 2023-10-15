fix bug of sign in pixel recon

domainlab/models/model_diva.py
@@ -178,10 +178,13 @@ def mk_diva(parent_class=VAEXYDClassif, str_diva_multiplier_type="default"):   #
 
     class ModelDIVAGammadReconPerPixel(ModelDIVAGammadRecon):
         def cal_reg_loss(self, tensor_x, tensor_y, tensor_d, others=None):
-            [loss_recon_x, zd_p_minus_zd_q, zx_p_minus_zx_q, zy_p_minus_zy_q, lc_d], [mu_recon, beta_d, beta_x, beta_y, gamma_d] = super().cal_reg_loss(tensor_x, tensor_y, tensor_d, others)
+            [loss_recon_x, zd_p_minus_zd_q, zx_p_minus_zx_q, zy_p_minus_zy_q, lc_d], [mu_recon, minus_beta_d, minus_beta_x, minus_beta_y, gamma_d] = super().cal_reg_loss(tensor_x, tensor_y, tensor_d, others)
 
-            return [torch.div(loss_recon_x, tensor_x.shape[2] * tensor_x.shape[3]), zd_p_minus_zd_q, zx_p_minus_zx_q, zy_p_minus_zy_q, lc_d], \
-                   [mu_recon, -beta_d, -beta_x, -beta_y, gamma_d]
+            return [torch.div(loss_recon_x,
+                              tensor_x.shape[2] * tensor_x.shape[3]),
+                    zd_p_minus_zd_q, zx_p_minus_zx_q, zy_p_minus_zy_q, lc_d], \
+                   [mu_recon, minus_beta_d, minus_beta_x,
+                    minus_beta_y, gamma_d]
 
     class ModelDIVAGammad(ModelDIVA):
         """