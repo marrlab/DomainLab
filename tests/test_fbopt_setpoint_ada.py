5aab629bd8c1cca26cb72b45fd12518ddc06c336
Xudong Sun <smilesun@users.noreply.github.com>
2023-10-15 19:13:56 +0200

    Create test_fbopt_setpoint_ada.py

---
+++ tests/test_fbopt_setpoint_ada.py
@@ -0,0 +1,7 @@
+from domainlab.algos.trainers.fbopt_setpoing_ada import is_less_list_all
+def test_less_than():
+  a = [3, 4, -9, -8]
+  b = [1, 0.5, -1, -0.5]
+  c = [0.5, 0.25, -0.5, -0.25]
+  assert not is_less_list_all(a, b)
+  assert is_less_list_all(c, b)
