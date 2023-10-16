dd3ed5518f6d336571c558e14209aa57f9d0a409
smilesun <smilesun.east@gmail.com>
2023-10-16 16:20:44 +0200

    fbopt_msel_setpoint

--- domainlab/algos/trainers/fbopt_mu_controller.py
+++ domainlab/algos/trainers/fbopt_mu_controller.py
@@ -44,12 +44,14 @@ class HyperSchedulerFeedback():
         # force initial value of mu
         self.mmu = {key: self.init_mu for key, val in self.mmu.items()}
 
-        self.set_point_controller = FbOptSetpointController(args=self.trainer.aconf)
+        self.set_point_controller = FbOptSetpointController(
+            args=self.trainer.aconf)
 
         self.k_i_control = trainer.aconf.k_i_gain
         self.overshoot_rewind = trainer.aconf.overshoot_rewind == "yes"
         self.delta_epsilon_r = None
-        # NOTE: this value will be set according to initial evaluation of neural network
+        # NOTE: this value will be set according to initial evaluation of
+        # neural network
         self.activation_clip = trainer.aconf.exp_shoulder_clip
         if trainer.aconf.no_tensorboard:
             self.writer = StubSummaryWriter()
@@ -90,24 +92,32 @@ class HyperSchedulerFeedback():
         """
         ma of delta
         """
-        return [(1 - coeff) * a + coeff * b for a, b in zip(list_old, list_new)]
+        return [(1 - coeff) * a + coeff * b
+                for a, b in zip(list_old, list_new)]
 
-    def tackle_overshoot(self, activation, epo_reg_loss, list_str_multiplier_na):
+    def tackle_overshoot(self,
+                         activation, epo_reg_loss, list_str_multiplier_na):
         """
         tackle overshoot
         """
-        list_overshoot = [i if (a - b) * (self.delta_epsilon_r[i]) < 0 else None
+        list_overshoot = [i if (a - b) * (self.delta_epsilon_r[i]) < 0
+                          else None
                           for i, (a, b) in
-                          enumerate(zip(epo_reg_loss, self.set_point_controller.setpoint4R))]
+                          enumerate(
+                              zip(epo_reg_loss,
+                                  self.set_point_controller.setpoint4R))]
         for ind in list_overshoot:
             if ind is not None:
-                logger = Logger.get_logger(logger_name='main_out_logger', loglevel="INFO")
+                logger = Logger.get_logger(
+                    logger_name='main_out_logger', loglevel="INFO")
                 logger.info(f"error integration: {self.delta_epsilon_r}")
-                logger.info(f"overshooting at  pos {ind} of activation: {activation}")
+                logger.info(f"overshooting at  pos \
+                            {ind} of activation: {activation}")
                 logger.info(f"name reg loss:{list_str_multiplier_na}")
                 if self.overshoot_rewind:
                     activation[ind] = 0.0
-                    logger.info(f"PID controller set to zero now, new activation: {activation}")
+                    logger.info(f"PID controller set to zero now, \
+                                new activation: {activation}")
         return activation
 
     def cal_activation(self):
@@ -116,12 +126,14 @@ class HyperSchedulerFeedback():
         """
         setpoint = self.get_setpoing4r()
         activation = [self.k_i_control * val if setpoint[i] > 0
-                      else self.k_i_control * (-val) for i, val in enumerate(self.delta_epsilon_r)]
+                      else self.k_i_control * (-val) for i, val
+                      in enumerate(self.delta_epsilon_r)]
         if self.activation_clip is not None:
-            activation = [np.clip(val, a_min=-1 * self.activation_clip, a_max=self.activation_clip)
+            activation = [np.clip(val,
+                                  a_min=-1 * self.activation_clip,
+                                  a_max=self.activation_clip)
                           for val in activation]
         return activation
-
 
     def search_mu(self, epo_reg_loss, epo_task_loss, epo_loss_tr,
                   list_str_multiplier_na, miter):
@@ -131,29 +143,37 @@ class HyperSchedulerFeedback():
         to see if the criteria is met
         $$\\mu^{k+1}=mu^{k}exp(rate_mu*[R(\\theta^{k})-ref_R])$$
         """
-        logger = Logger.get_logger(logger_name='main_out_logger', loglevel="INFO")
+        logger = Logger.get_logger(
+            logger_name='main_out_logger', loglevel="INFO")
         logger.info(f"before controller: current mu: {self.mmu}")
         logger.info(f"epo reg loss: {epo_reg_loss}")
         logger.info(f"name reg loss:{list_str_multiplier_na}")
         self.cal_delta4control(epo_reg_loss, self.get_setpoing4r())
         activation = self.cal_activation()
         # overshoot handling
-        activation = self.tackle_overshoot(activation, epo_reg_loss, list_str_multiplier_na)
+        activation = self.tackle_overshoot(
+            activation, epo_reg_loss, list_str_multiplier_na)
         list_gain = np.exp(activation)
         dict_gain = dict(zip(list_str_multiplier_na, list_gain))
         target = self.dict_multiply(self.mmu, dict_gain)
         self.mmu = self.dict_clip(target)
-        logger = Logger.get_logger(logger_name='main_out_logger', loglevel="INFO")
+        logger = Logger.get_logger(
+            logger_name='main_out_logger', loglevel="INFO")
         logger.info(f"after contoller: current mu: {self.mmu}")
 
         for key, val in self.mmu.items():
             self.writer.add_scalar(f'dyn_mu/{key}', val, miter)
-            self.writer.add_scalar(f'controller_gain/{key}', dict_gain[key], miter)
+            self.writer.add_scalar(
+                f'controller_gain/{key}', dict_gain[key], miter)
             ind = list_str_multiplier_na.index(key)
-            self.writer.add_scalar(f'delta/{key}', self.delta_epsilon_r[ind], miter)
-        for i, (reg_dyn, reg_set) in enumerate(zip(epo_reg_loss, self.get_setpoing4r())):
-            self.writer.add_scalar(f'lossrd/dyn_{list_str_multiplier_na[i]}', reg_dyn, miter)
-            self.writer.add_scalar(f'lossrs/setpoint_{list_str_multiplier_na[i]}', reg_set, miter)
+            self.writer.add_scalar(
+                f'delta/{key}', self.delta_epsilon_r[ind], miter)
+        for i, (reg_dyn, reg_set) in \
+                enumerate(zip(epo_reg_loss, self.get_setpoing4r())):
+            self.writer.add_scalar(
+                f'lossrd/dyn_{list_str_multiplier_na[i]}', reg_dyn, miter)
+            self.writer.add_scalar(
+                f'lossrs/setpoint_{list_str_multiplier_na[i]}', reg_set, miter)
 
             self.writer.add_scalars(
                 f'loss_rds/loss_{list_str_multiplier_na[i]} with setpoint',
@@ -161,21 +181,26 @@ class HyperSchedulerFeedback():
                  f'lossr/setpoint_{list_str_multiplier_na[i]}': reg_set,
                  }, miter)
             self.writer.add_scalar(
-                f'x-axis=loss_ell task vs y-axis=loss r/dyn{list_str_multiplier_na[i]}',
+                f'x-axis=loss_ell task vs y-axis=loss \
+                r/dyn{list_str_multiplier_na[i]}',
                 reg_dyn, epo_task_loss)
         self.writer.add_scalar('loss_task/penalized', epo_loss_tr, miter)
         self.writer.add_scalar('loss_task/ell', epo_task_loss, miter)
         acc_te = 0
         acc_val = 0
         acc_sel = 0
+        acc_set = 0
 
         if miter > 1:
             acc_te = self.trainer.observer.metric_te["acc"]
             acc_val = self.trainer.observer.metric_val["acc"]
             acc_sel = self.trainer.observer.model_sel.sel_model_te_acc
+            acc_set = \
+                self.trainer.observer.model_sel.oracle_last_setpoint_sel_te_acc
         self.writer.add_scalar("acc/te", acc_te, miter)
         self.writer.add_scalar("acc/val", acc_val, miter)
         self.writer.add_scalar("acc/sel", acc_sel, miter)
+        self.writer.add_scalar("acc/setpoint", acc_set, miter)
 
     def dict_clip(self, dict_base):
         """
@@ -197,7 +222,8 @@ class HyperSchedulerFeedback():
         """
         multiply a float to each element of a dictionary
         """
-        return {key: val * dict_multiplier[key] for key, val in dict_base.items()}
+        return {
+            key: val * dict_multiplier[key] for key, val in dict_base.items()}
 
     def update_setpoint(self, epo_reg_loss, epo_task_loss):
         """
