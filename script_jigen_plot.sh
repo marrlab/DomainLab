d0119e436c428c7e24cdf8d8d7db95c3d0fa5b70
smilesun <smilesun.east@gmail.com>
2023-10-26 17:43:43 +0200

    .

--- script_jigen_plot.sh
+++ script_jigen_plot.sh
@@ -1,5 +1,5 @@
 python domainlab/utils/generate_fbopt_phase_portrait.py --plot2="lossrd/dyn_
-alpha" --plot1="loss_task/ell" --label2="regularization loss jigen" --label1="classification loss" --output_dir="." --phase_portrait                                                                      
+alpha" --plot1="loss_task/ell" --legend2="regularization loss jigen" --legend1="classification loss" --output_dir="." --phase_portrait                                                                      
 
 
-python domainlab/utils/generate_fbopt_phase_portrait.py --plot2="lossrd/dyn_alpha" --plot1="lossrs/setpoint_alpha" --label2="regularization loss jigen" --label1="setpoint" --output_dir="."
+python domainlab/utils/generate_fbopt_phase_portrait.py --plot2="lossrd/dyn_alpha" --plot1="lossrs/setpoint_alpha" --legend2="regularization loss jigen" --legend1="setpoint" --output_dir="."
