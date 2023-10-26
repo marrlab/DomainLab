64695d286144386b31a78c3c66c047da72070a92
smilesun <smilesun.east@gmail.com>
2023-10-26 17:03:15 +0200

    .

--- script_generate_all_figures_diva.sh
+++ script_generate_all_figures_diva.sh
@@ -24,10 +24,10 @@ python domainlab/utils/generate_fbopt_phase_portrait.py --plot1="lossrd/dyn_beta
 python domainlab/utils/generate_fbopt_phase_portrait.py --plot1="lossrd/dyn_beta_y" --plot2="lossrs/setpoint_beta_y" --legend1="KL (beta_y)" --legend2="setpoint" --output_dir="./figures_diva"
 
 
-# # Other plots (one curve per figure)
-# values=('controller_gain/beta_d' 'controller_gain/beta_y' 'controller_gain/beta_x' 'controller_gain/gamma_d' 'controller_gain/mu_recon' 'dyn_mu/beta_d' 'delta/beta_d' 'dyn_mu/beta_y' 'delta/beta_y' 'dyn_mu/beta_x' 'delta/beta_x' 'dyn_mu/gamma_d' 'delta/gamma_d' 'dyn_mu/mu_recon' 'delta/mu_recon' 'loss_task/penalized' 'loss_task/ell' 'acc/te' 'acc/val' 'acc/sel' 'acc/setpoint')
-# # Loop over the array
-# for val in "${values[@]}"
-# do
-#   python domainlab/utils/generate_fbopt_phase_portrait.py --plot1="$val" --legend1="$val" --output_dir="./figures_diva"
-# done
+ # Other plots (one curve per figure)
+ values=('controller_gain/beta_d' 'controller_gain/beta_y' 'controller_gain/beta_x' 'controller_gain/gamma_d' 'controller_gain/mu_recon' 'dyn_mu/beta_d' 'delta/beta_d' 'dyn_mu/beta_y' 'delta/beta_y' 'dyn_mu/beta_x' 'delta/beta_x' 'dyn_mu/gamma_d' 'delta/gamma_d' 'dyn_mu/mu_recon' 'delta/mu_recon' 'loss_task/penalized' 'loss_task/ell' 'acc/te' 'acc/val' 'acc/sel' 'acc/setpoint')
+ # Loop over the array
+ for val in "${values[@]}"
+ do
+   python domainlab/utils/generate_fbopt_phase_portrait.py --plot1="$val" --legend1="$val" --output_dir="./figures_diva"
+ done
