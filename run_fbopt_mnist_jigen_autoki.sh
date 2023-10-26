c58d092b97459041e870f228333d53e3ffbe9c4d
smilesun <smilesun.east@gmail.com>
2023-10-26 11:36:41 +0200

    script for test

---
+++ run_fbopt_mnist_jigen_autoki.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# export CUDA_VISIBLE_DEVICES=""   
+# although garbage collector has been explicitly called, sometimes there is still CUDA out of memory error
+# so it is better not to use GPU to do the pytest to ensure every time there is no CUDA out of memory error occuring
+# pytest -s tests/test_fbopt.py
+
+python main_out.py --te_d=0 --tr_d 1 2 --task=mnistcolor10 --bs=16 --aname=jigen --trainer=fbopt --nname=conv_bn_pool_2 --epos=2000 --es=50 --mu_init=0.00001 --coeff_ma_setpoint=0.5 --coeff_ma_output_state=0.99 --k_i_gain_ratio=0.5
