7ea1ca129076b9c40a055fc28e9233526373c4e8
smilesun <smilesun.east@gmail.com>
2023-11-29 12:43:09 +0100

    reuse trepoch from base trainer

--- domainlab/algos/trainers/train_basic.py
+++ domainlab/algos/trainers/train_basic.py
@@ -28,8 +28,8 @@ class TrainerBasic(AbstractTrainer):
         self.epo_loss_tr = 0
         self.epo_reg_loss_tr = [0.0 for _ in range(10)]
         self.epo_task_loss_tr = 0
-        for ind_batch, (tensor_x, vec_y, vec_d, *others) in enumerate(self.loader_tr):
-            self.tr_batch(tensor_x, vec_y, vec_d, others, ind_batch, epoch)
+        for ind_batch, (tensor_x, tensor_y, tensor_d, *others) in enumerate(self.loader_tr):
+            self.tr_batch(tensor_x, tensor_y, tensor_d, others, ind_batch, epoch)
         self.epo_loss_tr /= self.counter_batch
         self.epo_task_loss_tr /= self.counter_batch
         self.epo_reg_loss_tr = list_divide(self.epo_reg_loss_tr, self.counter_batch)
@@ -40,22 +40,32 @@ class TrainerBasic(AbstractTrainer):
         return flag_stop
 
     def handle_r_loss(self, list_b_reg_loss):
+        """
+        just for logging
+        """
         list_b_reg_loss_sumed = [ele.sum().detach().item() for ele in list_b_reg_loss]
         self.epo_reg_loss_tr = list(map(add, self.epo_reg_loss_tr, list_b_reg_loss_sumed))
         return list_b_reg_loss_sumed
 
-    def tr_batch(self, tensor_x, vec_y, vec_d, others, ind_batch, epoch):
+    def cal_reg_loss(self, tensor_x, tensor_y, tensor_d, others=None):
+        return self._cal_reg_loss(tensor_x, tensor_y, tensor_d, others)
+
+    def tr_batch(self, tensor_x, tensor_y, tensor_d, others, ind_batch, epoch):
         """
         different from self.train_batch(...), which is used for mldg, the current function
         is used inside tr_epoch
         """
         self.before_batch(epoch, ind_batch)
-        tensor_x, vec_y, vec_d = \
-            tensor_x.to(self.device), vec_y.to(self.device), vec_d.to(self.device)
+        tensor_x, tensor_y, tensor_d = \
+            tensor_x.to(self.device), tensor_y.to(self.device), tensor_d.to(self.device)
         self.optimizer.zero_grad()
-        loss, list_loss_reg, loss_task = self.model.cal_loss(tensor_x, vec_y, vec_d, others)
+        loss, list_loss_reg, loss_task = self.model.cal_loss(tensor_x, tensor_y, tensor_d, others)
+        #
+        list_reg_tr, list_mu_tr = self.cal_reg_loss(tensor_x, tensor_y, tensor_d, others)
+        #
         self.handle_r_loss(list_loss_reg)
-        loss = loss.sum()
+        reg_tr = self.model.inner_product(list_reg_tr, list_mu_tr)
+        loss = loss.sum() +  reg_tr.sum()
         loss.backward()
         self.optimizer.step()
         self.epo_loss_tr += loss.detach().item()
@@ -63,16 +73,16 @@ class TrainerBasic(AbstractTrainer):
         self.after_batch(epoch, ind_batch)
         self.counter_batch += 1
 
-    def train_batch(self, tensor_x, vec_y, vec_d, others):
+    def train_batch(self, tensor_x, tensor_y, tensor_d, others):
         """
         use a temporary optimizer to update only the model upon a batch of data
         """
         # temparary optimizer
         optimizer = mk_opt(self.model, self.aconf)
-        tensor_x, vec_y, vec_d = \
-            tensor_x.to(self.device), vec_y.to(self.device), vec_d.to(self.device)
+        tensor_x, tensor_y, tensor_d = \
+            tensor_x.to(self.device), tensor_y.to(self.device), tensor_d.to(self.device)
         optimizer.zero_grad()
-        loss, *_ = self.model.cal_loss(tensor_x, vec_y, vec_d, others)
+        loss, *_ = self.model.cal_loss(tensor_x, tensor_y, tensor_d, others)
         loss = loss.sum()
         loss.backward()
         optimizer.step()
