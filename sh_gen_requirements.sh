6a0d843056baaf8087c25472c0a0fa0f7ddd45bb
Xudong Sun <smilesun@users.noreply.github.com>
2023-12-02 23:48:40 +0100

    Update sh_gen_requirements.sh

--- sh_gen_requirements.sh
+++ sh_gen_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 poetry update
-poetry export -f requirements.txt --output requirements.txt --without-hashes --without-urls
+poetry export -f requirements.txt --output requirements.txt --without-hashes --without-urls --without snakemake diatrie
