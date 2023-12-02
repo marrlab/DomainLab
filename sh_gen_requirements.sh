f53464795e7f53368516f539ddb2008431018f07
Xudong Sun <smilesun@users.noreply.github.com>
2023-12-02 23:59:58 +0100

    Update sh_gen_requirements.sh

--- sh_gen_requirements.sh
+++ sh_gen_requirements.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 poetry update
 poetry export -f requirements.txt --output requirements.txt --without-hashes --without-urls --without snakemake diatrie
+
+# only snamkemake poetry export --without-hashes -f requirements.txt | grep -v colorama > requirements.txt
