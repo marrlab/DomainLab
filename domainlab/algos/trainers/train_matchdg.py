c007ed32a2fccddb2ca20ed659b1d9df6de36fa0
Xudong Sun <smilesun@users.noreply.github.com>
2023-12-02 17:41:22 +0000

    loger

--- domainlab/algos/trainers/train_matchdg.py
+++ domainlab/algos/trainers/train_matchdg.py
@@ -23,12 +23,13 @@ class TrainerMatchDG(MatchCtrErm):
         """
         configure trainer accoding to properties of task as well according to algorithm configs
         """
+        logger = Logger.get_logger()
+        logger.info("\n\nPhase 1 start: contractive alignment without task loss: \n\n")
         # phase 1: contrastive learning
         # different than phase 2, ctr_model has no classification loss
         for epoch in range(self.aconf.epochs_ctr):
             self.tr_epoch(epoch)
-        logger = Logger.get_logger()
-        logger.info(f"Phase 1 finished: {self.model_path_ctr}")
+        logger.info(f"\n\nPhase 1 finished: {self.model_path_ctr}\n\n")
         # phase 2: ERM, initialize object
         self.observer.reset()
         self.aconf.epos = self.aconf.epos - self.aconf.epochs_ctr
