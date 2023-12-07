574804c3227e6857c220ce488475b49398c1dc91
smilesun <smilesun.east@gmail.com>
2023-12-07 12:56:29 +0100

    comment

--- sh_list_error.sh
+++ sh_list_error.sh
@@ -1,2 +1,4 @@
 # find $1 -type f -print0 | xargs -0 grep -li error 
-grep -B 20 -wnr "error" --group-separator="========================" $1
+# B means before, A means after, some erros have long stack exception message so we need at least 
+# 100 lines before the error, the last line usually indicate the root cause of error
+grep -B 100 -wnr "error" --group-separator="========================" $1
