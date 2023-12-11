7869ed124bb3a6b1e0650af5893d9141fbe8cbc6
smilesun <smilesun.east@gmail.com>
2023-12-11 16:26:26 +0100

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='domainlab',
     packages=find_packages(),
-    version='0.1.6',
+    version='0.1.7',
     description='Library of Domain Generalization',
     author='Xudong Sun, et.al.',
     license='MIT',
