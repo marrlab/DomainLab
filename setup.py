4295f2409e9de65cca2da8f6099d981ca0f94ffe
smilesun <smilesun.east@gmail.com>
2023-12-16 12:52:45 +0100

    nperm release 0.2.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     package_data =   {
                 'zdata': [f for f in copy_dir()],
              },
-    version='0.2.1',
+    version='0.2.2',
     description='Library of Domain Generalization',
     url='https://github.com/marrlab/DomainLab',
     author='Xudong Sun, et.al.',
