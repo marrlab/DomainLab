feacd2363e99a7772e333088d166ffa49733a7e5
smilesun <smilesun.east@gmail.com>
2023-12-17 10:50:36 +0100

    remove space

--- tests/test_decorate_model_cmd.py
+++ tests/test_decorate_model_cmd.py
@@ -8,7 +8,7 @@ def test_cmd_model_decorator_diva():
     """
     trainer decorator
     """
-    args = "--te_d=0 --task=mnistcolor10 --model=deepall, diva\
+    args = "--te_d=0 --task=mnistcolor10 --model=deepall,diva\
         --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 \
         --gamma_y=10e5 --gamma_d=1e5"
     utils_test_algo(args)
