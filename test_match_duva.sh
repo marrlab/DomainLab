233c311d7770db0d9b0885e6c638461920829e64
smilesun <smilesun.east@gmail.com>
2023-12-22 10:56:43 +0100

    merge hduva nn name change

--- test_match_duva.sh
+++ test_match_duva.sh
@@ -1,6 +1,6 @@
 python main_out.py --te_d 0 1 2 --tr_d 3 7 --task=mnistcolor10 --debug --bs=2 --model=matchhduva \
   --epochs_ctr=3 --epos=6 --nname=conv_bn_pool_2 --gamma_y=7e5 \
-        --nname_topic_distrib_img2topic=conv_bn_pool_2 \
-        --nname_encoder_sandwich_layer_img2h4zd=conv_bn_pool_2
+        --nname_encoder_x2topic_h=conv_bn_pool_2 \
+        --nname_encoder_sandwich_x2h4zd=conv_bn_pool_2
 
 
