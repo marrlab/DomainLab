02faaf9a7eed47752ded14195a3581c5512bfbff
smilesun <smilesun.east@gmail.com>
2024-01-04 11:47:25 +0100

    use stable official softplus

--- domainlab/compos/vae/compos/encoder_dirichlet.py
+++ domainlab/compos/vae/compos/encoder_dirichlet.py
@@ -13,15 +13,16 @@ class EncoderH2Dirichlet(nn.Module):
         """
         super().__init__()
         self.layer_bn = nn.BatchNorm1d(dim_topic)
+        self.layer_concentration = nn.Softplus()
         self.device = device
 
     def forward(self, hidden):
         """
         :param hidden:
         """
-        # feat_bnorm = self.layer_bn(hidden)
-        feat_bnorm = hidden
-        alphas_batch = torch.log(1 + feat_bnorm.exp())
+        feat_bnorm = self.layer_bn(hidden)
+        # alphas_batch = torch.log(1 + feat_bnorm.exp())
+        alphas_batch = self.layer_concentration(feat_bnorm)
         q_topic = Dirichlet(alphas_batch + 1e-6)
         topic_q = q_topic.rsample().to(self.device)
         return q_topic, topic_q
