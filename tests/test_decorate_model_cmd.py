6dc2d4ed597794eebbed85aa2ccd85da03207982
Xudong Sun <smilesun@users.noreply.github.com>
2024-01-08 22:04:20 +0100

    Update test_decorate_model_cmd.py

--- tests/test_decorate_model_cmd.py
+++ tests/test_decorate_model_cmd.py
@@ -4,11 +4,20 @@ end to end test for trainer decorator
 from tests.utils_test import utils_test_algo
 
 
-def test_cmd_model_decorator_diva():
+def test_cmd_model_erm_decorator_diva():
     """
     trainer decorator
     """
     args = "--te_d=0 --task=mnistcolor10 --model=erm,diva\
+        --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 \
+        --gamma_y=10e5 --gamma_d=1e5"
+    utils_test_algo(args)
+
+def test_cmd_model_dann_decorator_diva():
+    """
+    trainer decorator
+    """
+    args = "--te_d=0 --task=mnistcolor10 --model=dann,diva\
         --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 \
         --gamma_y=10e5 --gamma_d=1e5"
     utils_test_algo(args)
