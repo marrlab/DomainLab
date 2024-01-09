9dfc9424059194d4031cad1ab59f7bc6b5931f87
smilesun <smilesun.east@gmail.com>
2024-01-09 09:23:01 +0100

    change model name

--- tests/test_decorate_model_cmd.py
+++ tests/test_decorate_model_cmd.py
@@ -8,7 +8,7 @@ def test_cmd_model_erm_decorator_diva():
     """
     trainer decorator
     """
-    args = "--te_d=0 --task=mnistcolor10 --model=erm,diva\
+    args = "--te_d=0 --task=mnistcolor10 --model=erm_diva\
         --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 \
         --gamma_y=10e5 --gamma_d=1e5"
     utils_test_algo(args)
@@ -17,7 +17,7 @@ def test_cmd_model_dann_decorator_diva():
     """
     trainer decorator
     """
-    args = "--te_d=0 --task=mnistcolor10 --model=dann,diva\
+    args = "--te_d=0 --task=mnistcolor10 --model=dann_diva\
         --nname=conv_bn_pool_2 --nname_dom=conv_bn_pool_2 \
         --gamma_y=10e5 --gamma_d=1e5"
     utils_test_algo(args)
