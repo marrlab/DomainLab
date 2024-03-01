f3e9d0faf725914942b69134ba01e00074acc14b
smilesun <smilesun.east@gmail.com>
2024-03-01 10:18:48 +0100

    comment how setpoint model selction works

--- domainlab/algos/msels/c_msel_setpoint_delay.py
+++ domainlab/algos/msels/c_msel_setpoint_delay.py
@@ -29,15 +29,24 @@ class MSelSetpointDelay(AMSel):
     def update(self, clear_counter=False):
         """
         if the best model should be updated
+        currently, clear_counter is set via
+        flag = super().tr_epoch(epoch, self.flag_setpoint_updated)
         """
         logger = Logger.get_logger()
         logger.info(
             f"setpoint selected current acc {self._oracle_last_setpoint_sel_te_acc}"
         )
         if clear_counter:
+            # for the current version of code, clear_counter = flag_setpoint_updated
             log_message = (
                 f"setpoint msel te acc updated from "
+                # self._oracle_last_setpoint_sel_te_acc start from 0.0, and always saves
+                # the test acc when last setpoint decrease occurs
                 f"{self._oracle_last_setpoint_sel_te_acc} to "
+                # self.sel_model_te_acc defined as a property
+                # in a_msel, which returns self.msel.sel_model_te_acc
+                # is the validation acc based model selection, which
+                # does not take setpoint into account
                 f"{self.sel_model_te_acc}"
             )
             logger.info(log_message)
