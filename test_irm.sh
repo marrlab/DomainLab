ef16d066889f5fd52b533882da7284518579156b
smilesun <smilesun.east@gmail.com>
2024-05-13 10:48:31 +0200

    fix codacy

--- test_irm.sh
+++
@@ -1,1 +0,0 @@
-python main_out.py --te_d=0 --task=mnistcolor10 --keep_model --model=erm --trainer=irm --nname=conv_bn_pool_2
--- tests/test_irm.py
+++ tests/test_irm.py
@@ -1,13 +1,22 @@
 """
-unit and end-end test for deep all, mldg
+end-end test
 """
 from tests.utils_test import utils_test_algo
 
 
-def test_deepall_mldg():
+def test_irm():
     """
-    train DeepAll with MLDG
+    train with Invariant Risk Minimization
     """
     args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm \
         --trainer=irm --nname=alexnet"
+    utils_test_algo(args)
+
+
+def test_irm_mnist():
+    """
+    train with Invariant Risk Minimization
+    """
+    args = "--te_d=0 --task=mnistcolor10 --keep_model --model=erm \
+        --trainer=irm --nname=conv_bn_pool_2"
     utils_test_algo(args)
