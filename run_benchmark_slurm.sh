8d6a356369c154c187d90aef55c6da5158166d3b
smilesun <smilesun.east@gmail.com>
2024-07-05 17:06:22 +0200

    use commandline to specify enviornemnt variable

--- run_benchmark_slurm.sh
+++ run_benchmark_slurm.sh
@@ -32,4 +32,4 @@ echo "Number of GPUs: $NUMBER_GPUS"
 echo "Results will be stored in: $results_dir"
 
 # Helmholtz
-snakemake --profile "examples/yaml/slurm" --config yaml_file="$CONFIGFILE" --keep-going --keep-incomplete --notemp --cores 3 -s "domainlab/exp_protocol/benchmark.smk" --configfile "$CONFIGFILE" --config output_dir="$results_dir" 2>&1 | tee "$logfile"
+snakemake --config logdir="zoutput/benchmark/logs" --profile "examples/yaml/slurm" --config yaml_file="$CONFIGFILE" --keep-going --keep-incomplete --notemp --cores 3 -s "domainlab/exp_protocol/benchmark.smk" --configfile "$CONFIGFILE" --config output_dir="$results_dir" 2>&1 | tee "$logfile"
