97112a2827d08bad5bdc8ce5ed3476706726a3c9
smilesun <smilesun.east@gmail.com>
2024-07-18 16:57:37 +0200

    move avarage rho set w.r.t. epoch

--- domainlab/algos/trainers/train_ema.py
+++ domainlab/algos/trainers/train_ema.py
@@ -1,26 +1,69 @@
+"""
+simple exponential moving average of each layers, after each epoch,
+trainer=ma_trainer2_trainer3
+always set ma to be outer most
+
+Paper:
+Ensemble of Averages: Improving Model Selection and
+Boosting Performance in Domain Generalization
+Devansh Arpit, Huan Wang, Yingbo Zhou, Caiming Xiong
+Salesforce Research, USA
+"""
+
 import torch
+from domainlab.algos.trainers.train_basic import TrainerBasic
 
 
-class MovingAverage:
-    def __init__(self, rho):
-        self.rho = rho
-        self.dict_para_ema_persist = {}
-        self._iter = 0
+class TrainerMA(TrainerBasic):
+    """
+    initializer of this class goes to one block/section in the abstract class
+    initializer, otherwise it will break the class inheritance.
+    """
+    def move_average(self, dict_data, epoch):
+        """
+        for each epoch, convex combine the weights for each layer
+        """
+        if self.ma_weight_previous_model_params is None:
+            """
+            Paper:
+            Ensemble of Averages: Improving Model Selection and
+            Boosting Performance in Domain Generalization
+            Devansh Arpit, Huan Wang, Yingbo Zhou, Caiming Xiong
+            Salesforce Research, USA
 
-    def update(self, dict_data):
+            """
+            if epoch < 2:
+                # see https://arxiv.org/pdf/2110.10832#page=13.16, figure 5
+                self.ma_weight_previous_model_params = 0
+            else:
+                self.ma_weight_previous_model_params = epoch / (epoch + 1)
+            # weight on previous model decrease as training goes on
         dict_ema_para_curr_iter = {}
         for key, data in dict_data.items():
-            data = data.view(1, -1)  # make it rank 1 tensor (a.k.a. vector)
-            if self._iter == 0:
+            # data = data.view(1, -1)  # make it rank 1 tensor (a.k.a. vector)
+            if self._ma_iter == 0:
                 previous_data = torch.zeros_like(data)
             else:
-                previous_data = self.dict_para_ema_persist[key]
+                previous_data = self._ma_dict_para_persist[key]
 
-            local_data_convex = self.rho * previous_data + (1 - self.rho) * data
+            local_data_convex = \
+                self.ma_weight_previous_model_params * previous_data + \
+                (1 - self.ma_weight_previous_model_params) * data
             # correction by 1/(1 - self.rho)
-            # so that the gradients amplitude backpropagated in data is independent of self.rho
-            dict_ema_para_curr_iter[key] = local_data_convex / (1 - self.rho)
-            self.dict_para_ema_persist[key] = local_data_convex.clone().detach()  # used as previous data
+            # so that the gradients amplitude backpropagated in data is
+            # independent of self.rho
+            dict_ema_para_curr_iter[key] = \
+                local_data_convex / (1 - self.ma_weight_previous_model_params)
+            self._ma_dict_para_persist[key] = \
+                local_data_convex.clone().detach()  # used as previous data
 
-        self._iter += 1
+        self._ma_iter += 1
         return dict_ema_para_curr_iter
+
+    def after_epoch(self, epoch):
+        torch_model = self.get_model()
+        dict_para = torch_model.state_dict()  # only for trainable parameters
+        new_dict_para = self.move_average(dict_para, epoch)
+        # without deepcopy, this seems to work
+        torch_model.load_state_dict(new_dict_para)
+        super().after_epoch(epoch)
