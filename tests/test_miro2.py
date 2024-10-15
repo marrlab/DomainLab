8d3402cb3de19e1a753698c6ca5e8efab3745d1f
smilesun <smilesun.east@gmail.com>
2024-10-15 12:14:40 +0200

    .

--- tests/test_miro2.py
+++ tests/test_miro2.py
@@ -4,10 +4,11 @@ end-end test for mutual information regulation
 from tests.utils_test import utils_test_algo
 
 
-def test_miro():
+def test_miro2():
     """
     train with MIRO
     """
     args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm \
-        --trainer=miro --nname=alexnet"
+        --trainer=miro --nname=alexnet \
+        --layers2extract_feats _net_invar_feat.net_torchvision.features.1"
     utils_test_algo(args)
