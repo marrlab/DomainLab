8b73a814b2fb2507a4acce2582ec2b7bf591d1f3
Xudong Sun <smilesun@users.noreply.github.com>
2024-10-18 12:00:12 +0200

    Update sh_benchmark_agg.sh

--- scripts/sh_benchmark_agg.sh
+++ scripts/sh_benchmark_agg.sh
@@ -1,3 +1,4 @@
 cat $1/rule_results/*.csv > temp_results.csv
+sh reorganize_csv_with_repeated_headers.sh merged_data.csv
 mkdir -p temp_agg
-python main_out.py --gen_plots temp_results.csv --outp_dir ./temp_agg
+python main_out.py --gen_plots merged_data.csv --outp_dir ./temp_agg
