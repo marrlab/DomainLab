6f08403bbb92cab8398ce1154f9f28e573e0562c
smilesun <smilesun.east@gmail.com>
2024-12-06 13:08:18 +0100

    no_dump in test_fbopt.py

--- tests/test_fbopt.py
+++ tests/test_fbopt.py
@@ -9,7 +9,7 @@ def test_dann_fbopt():
     """
     dann
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=dann --trainer=fbopt --nname=alexnet --epos=3"
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=dann --trainer=fbopt --nname=alexnet --epos=3 --no_dump"
     utils_test_algo(args)
 
 
@@ -17,7 +17,7 @@ def test_jigen_fbopt():
     """
     jigen
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=jigen --trainer=fbopt --nname=alexnet --epos=3"
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=jigen --trainer=fbopt --nname=alexnet --epos=3 --no_dump"
     utils_test_algo(args)
 
 
@@ -25,7 +25,7 @@ def test_diva_fbopt():
     """
     diva
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=diva --gamma_y=1.0 --trainer=fbopt --nname=alexnet --epos=3"
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=diva --gamma_y=1.0 --trainer=fbopt --nname=alexnet --epos=3 --no_dump"
     utils_test_algo(args)
 
 
@@ -33,7 +33,7 @@ def test_erm_fbopt():
     """
     erm
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm --trainer=fbopt --nname=alexnet --epos=3" # pylint: disable=line-too-long
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm --trainer=fbopt --nname=alexnet --epos=3 --no_dump" # pylint: disable=line-too-long
     with pytest.raises(RuntimeError):
         utils_test_algo(args)
 
@@ -42,7 +42,7 @@ def test_irm_fbopt():
     """
     irm
     """
-    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm --trainer=fbopt_irm --nname=alexnet --epos=3" # pylint: disable=line-too-long
+    args = "--te_d=caltech --task=mini_vlcs --debug --bs=2 --model=erm --trainer=fbopt_irm --nname=alexnet --epos=3 --no_dump" # pylint: disable=line-too-long
     utils_test_algo(args)
 
 
@@ -50,5 +50,5 @@ def test_forcesetpoint_fbopt():
     """
     diva
     """
-    args = "--te_d=0 --tr_d 1 2 --task=mnistcolor10 --bs=16 --model=jigen --trainer=fbopt --nname=conv_bn_pool_2 --epos=10 --es=0 --mu_init=0.00001 --coeff_ma_setpoint=0.5 --coeff_ma_output_state=0.99 --force_setpoint_change_once"
+    args = "--te_d=0 --tr_d 1 2 --task=mnistcolor10 --bs=16 --model=jigen --trainer=fbopt --nname=conv_bn_pool_2 --epos=10 --es=0 --mu_init=0.00001 --coeff_ma_setpoint=0.5 --coeff_ma_output_state=0.99 --force_setpoint_change_once --no_dump"
     utils_test_algo(args)
