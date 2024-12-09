163ca911f4b7803f63404ccf376ed18e77108de7
Xudong Sun <smilesun@users.noreply.github.com>
2024-12-09 17:11:18 +0100

    Update sh_genplot.sh

--- scripts/sh_genplot.sh
+++ scripts/sh_genplot.sh
@@ -1,3 +1,3 @@
-mkdir $2
-merge_csvs.sh
-python main_out.py --gen_plots merged_data.csv  --outp_dir $2
+# mkdir $2
+sh scripts/merge_csvs.sh $1
+python main_out.py --gen_plots merged_data.csv  --outp_dir partial_agg_plots
